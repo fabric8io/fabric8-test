cd46735e429c9ab036ff775c14f6102d333b8851
ldimaggi <ldimaggi@redhat.com>
2017-09-06 11:08:55 -0400

    fix(test): add options to cleaning before running tests

--- ee_tests/src/tests/work-item/work-item-list/EE/openshift-io-burr-che.spec.js
+++ ee_tests/src/tests/work-item/work-item-list/EE/openshift-io-burr-che.spec.js
@@ -99,7 +99,7 @@ describe('openshift.io End-to-End POC test - Scenario - CREATE project - Run Che
 
     /* ----------------------------------------------------------*/
     /* Step 1) Login to openshift.io */
-    OpenShiftIoDashboardPage = testSupport.loginCleanUpdate (page, browser.params.login.user, browser.params.login.password );
+    OpenShiftIoDashboardPage = testSupport.loginCleanUpdate (page, browser.params.login.user, browser.params.login.password, constants.CLEAN_ALL );
 
     /* ----------------------------------------------------------*/
     /* Step 2) In OSIO, create new space */
