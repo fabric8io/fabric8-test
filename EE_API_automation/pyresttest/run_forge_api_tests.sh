964e5ec10ef062a4506a06cb6dc5f2f3971801af
Corinne Krych <corinnekrych@gmail.com>
2017-09-28 10:40:43 +0200

    feat(forge): Add quickstart test

--- EE_API_automation/pyresttest/run_forge_api_tests.sh
+++ EE_API_automation/pyresttest/run_forge_api_tests.sh
@@ -5,10 +5,10 @@ then
 else
     if [ -z "$1" ] 
     then
-        echo "No OSIO username supplied - usage: run_forge_api YOUR_OSIO_USERNAME YOUR_KEYCLOAK_TOKEN"
+        echo "No OSIO username supplied - usage: run_forge_api_tests YOUR_OSIO_USERNAME YOUR_KEYCLOAK_TOKEN"
     elif [ -z "$2" ] 
     then
-        echo "No OSIO auth token supplied - usage: run_forge_api YOUR_OSIO_USERNAME YOUR_KEYCLOAK_TOKEN"
+        echo "No OSIO auth token supplied - usage: run_forge_api_tests YOUR_OSIO_USERNAME YOUR_KEYCLOAK_TOKEN"
     else
         echo "==> 1. Check token is valid for Forge"
         isTokenValid=$(curl --header "Authorization: Bearer $2" https://forge.api.openshift.io/forge/commands/fabric8-import-git | grep -c 'java.lang.IllegalStateException')
@@ -19,12 +19,12 @@ else
         echo "==> 2. start pre-requisites: create space WIZARD..."
         pyresttest https://api.openshift.io get_a_space.yaml --vars="{'token': '$2', 'userid': '$1', 'space_name_var': 'WIZARD'}" #--interactive true #--print-headers true
         echo "==> 3. start forge api testing..."
-        #pyresttest https://api.openshift.io gihub_step.yaml --vars="{'token': '$2', 'userid': '$1'}" #--interactive true #--print-headers true 
         hasGitOrganisation=$(curl --header "Authorization: Bearer $2" https://forge.api.openshift.io/forge/commands/fabric8-import-git | grep -c 'GithubImportPickOrganisationStep')
         if [ "$hasGitOrganisation" -gt "0" ]; then 
             echo "==> Username belongs to a github organisation: $hasGitOrganisation"
-            pyresttest https://forge.api.openshift.io API_forge_wizard.yaml --vars="{'token': '$2', 'userid': '$1', 'space_name_var': 'WIZARD'}" #--interactive true --print-headers true
-
+            #pyresttest https://forge.api.openshift.io API_forge_wizard.yaml --vars="{'token': '$2', 'userid': '$1', 'space_name_var': 'WIZARD'}" #--interactive true --print-headers true
+            uuid=$(uuidgen) # TODO for ubuntu, use uuid-runtime 
+            pyresttest https://forge.api.openshift.io API_forge_quickstart_wizard.yaml --vars="{'token': '$2', 'userid': '$1', 'space_name_var': 'WIZARD', 'repo_name': quickstart-$uuid}" #--interactive true --print-headers true
         else
             echo "==> Username doesn't belong to any github organisation: $hasGitOrganisation"
             pyresttest https://forge.api.openshift.io API_forge_wizard_no_gh_organisation.yaml --vars="{'token': '$2', 'userid': '$1', 'space_name_var': 'WIZARD'}" #--interactive true --print-headers true
