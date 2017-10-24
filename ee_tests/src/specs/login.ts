b6c3a1c4b9a097df5f19bb8b2e782131745be92d
Sunil Thaha <sthaha@redhat.com>
2017-10-25 09:35:26 +1000

    Fix issues reported by tslint

--- ee_tests/src/specs/login.ts
+++ ee_tests/src/specs/login.ts
@@ -1,75 +1,67 @@
-import { browser, ExpectedConditions as EC, $, $$ } from 'protractor'
-import * as support from './support'
+import { browser, ExpectedConditions as EC, $, $$ } from 'protractor';
+import * as support from './support';
 
 export abstract class BasePage {
-  protected url: string = '' // Will be same as baseUrl by default.
+  protected url: string = ''; // Will be same as baseUrl by default.
 
   constructor(url?: string) {
-    this.url = url || ''
+    this.url = url || '';
   }
 
   async open() {
-		await browser.get(this.url)
+    await browser.get(this.url);
   }
 
 }
 
 export class HomePage extends BasePage {
-  login = $('#login')
+  login = $('#login');
 
   constructor(url: string) {
-    super(url)
+    super(url);
   }
 }
-
-//section > div > div.progress-description > div<Paste>
 
 export class LoginPage extends BasePage {
-  usernameInput = $('#username')
-  passwordInput = $('#password')
-  loginButton = $('#kc-login')
+  usernameInput = $('#username');
+  passwordInput = $('#password');
+  loginButton = $('#kc-login');
 
-  async login(username: string, password : string) {
-    await expect(this.usernameInput).toAppear('Username input must be present')
-    await expect(this.passwordInput).toAppear('Password input must be present')
-    await expect(this.loginButton).toAppear('Password input must be present')
+  async login(username: string, password: string) {
+    await expect(this.usernameInput).toAppear('Username input must be present');
+    await expect(this.passwordInput).toAppear('Password input must be present');
+    await expect(this.loginButton).toAppear('Password input must be present');
 
-    await this.usernameInput.sendKeys(username)
-    await this.passwordInput.sendKeys(password)
-    await this.loginButton.click()
+    await this.usernameInput.sendKeys(username);
+    await this.passwordInput.sendKeys(password);
+    await this.loginButton.click();
   }
 }
 
-// packaging
-// typescript
-// contract of who creates teh pgObject model
-//
+// declare let expect:any
 
-declare let expect:any
-
-describe('HomePage', ()=> {
+describe('HomePage', () => {
   let homePage: HomePage;
 
-  beforeEach( async (): Promise<any> => {
-    support.desktopTestSetup()
-		homePage = new HomePage(browser.params.target.url)
-    await homePage.open()
-  })
+  beforeEach( async () => {
+    support.desktopTestSetup();
+    homePage = new HomePage(browser.params.target.url);
+    await homePage.open();
+  });
 
-  it('shows the title', async ()=> {
-    await expect(browser.getTitle()).toEqual('OpenShift.io')
-  })
+  it('shows the title', async () => {
+    await expect(browser.getTitle()).toEqual('OpenShift.io');
+  });
 
-  it('show login button', async ()=>{
-		browser.ignoreSynchronization = true
-    await expect($$('div').first()).toAppear('Atleast one div should appear on the page')
-    await expect( homePage.login).toAppear('Login must be present')
-  })
+  it('show login button', async () => {
+    await expect($$('div').first()).toAppear('Atleast one div should appear on the page');
+    await expect( homePage.login).toAppear('Login must be present');
+  });
 
-  it('can login using correct username and password', async ()=>{
-    await homePage.login.click()
+  it('can login using correct username and password', async () => {
+    await homePage.login.click();
 
-    let loginPage = new LoginPage()
-    await loginPage.login(browser.params.login.user, browser.params.login.password)
-  })
-})
+    let loginPage = new LoginPage();
+    await loginPage.login(browser.params.login.user, browser.params.login.password);
+  });
+});
--- ee_tests/src/specs/support.ts
+++ ee_tests/src/specs/support.ts
@@ -1,34 +1,31 @@
-import { browser } from 'protractor'
+import { browser } from 'protractor';
 
 export enum BrowserMode {
-	Phone,
-	Tablet,
-	Desktop,
+  Phone,
+  Tablet,
+  Desktop
 }
 
 export function setBrowserMode(mode: BrowserMode): void {
 
-	let window = browser.driver.manage().window()
-    switch (mode) {
-	case BrowserMode.Phone:
-		window.setSize(430, 667);
-		break;
-	case BrowserMode.Tablet:
-		window.setSize(768, 1024);
-		break;
-    case BrowserMode.Desktop:
-		window.setSize(1920, 1080);
-	}
+  let window = browser.driver.manage().window();
+  switch (mode) {
+  case BrowserMode.Phone:
+    window.setSize(430, 667);
+    break;
+  case BrowserMode.Tablet:
+    window.setSize(768, 1024);
+    break;
+  case BrowserMode.Desktop:
+    window.setSize(1920, 1080);
+    break;
+  default:
+    throw Error('Unknown mode');
+  }
 }
 
 export function desktopTestSetup() {
-	browser.ignoreSynchronization = true
-	setBrowserMode(BrowserMode.Desktop)
+  browser.ignoreSynchronization = true;
+  setBrowserMode(BrowserMode.Desktop);
 }
 
-export async function restoreIgnoreSync(fn: ()=> any) {
-	const old = browser.ignoreSynchronization
-	browser.ignoreSynchronization = true
-	fn()
-	browser.ignoreSynchronization = old
-}
