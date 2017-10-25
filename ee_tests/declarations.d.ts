3c604ca03c735ef0d618d5d63521620094186089
Sunil Thaha <sthaha@redhat.com>
2017-10-25 11:02:09 +1000

    POC: protractor config ported to typescript

---
+++ ee_tests/declarations.d.ts
@@ -0,0 +1,4 @@
+declare module "HtmlScreenshotReporter";
+declare module "protractor-jasmine2-screenshot-reporter";
+declare module "jasmine-spec-reporter";
+declare module "jasmine-protractor-matchers";
