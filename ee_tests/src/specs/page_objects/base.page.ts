31823cde5664a215787150adb34a1c33231aa3e4
Sunil Thaha <sthaha@redhat.com>
2017-10-31 14:57:08 +1000

    Log Page and UI actions

--- ee_tests/src/specs/page_objects/base.page.ts
+++ ee_tests/src/specs/page_objects/base.page.ts
@@ -1,4 +1,5 @@
 import { browser } from 'protractor';
+import * as mixins from '../mixins';
 
 export abstract class BasePage {
   protected url: string = ''; // Will be same as baseUrl by default.
@@ -7,8 +8,17 @@ export abstract class BasePage {
     this.url = url || '';
   }
 
-  async open() {
-    await browser.get(this.url);
+  async ready() {
   }
 
+  async open() {
+    await this.ready();
+    this.log('Opened')
+  }
+
+  // add logging mixin
+  name: string = '...';
+  log: (action: string, ...msg: string[]) => void;
 }
+
+mixins.applyMixins(BasePage, [mixins.Logging]);
--- ee_tests/src/specs/page_objects/ui/base.element.ts
+++ ee_tests/src/specs/page_objects/ui/base.element.ts
@@ -1,19 +1,41 @@
 import { browser, ExpectedConditions as until, ElementFinder } from 'protractor';
+import { info } from '../../support';
+import * as mixins from '../../mixins'
 
 export class BaseElement extends ElementFinder {
+  name: string = ''
   /**
    * Extend this class, to describe single custom fragment on your page
    *
    * @param {ElementFinder} elementFinder ElementFinder that you want to extend
+   * @param {string} name to indentify the element in the logs
    */
-  constructor(elementFinder: ElementFinder) {
-      // Basically we are recreating ElementFinder again with same parameters
-      super(elementFinder.browser_, elementFinder.elementArrayFinder_);
+  constructor(elementFinder: ElementFinder, name: string = '') {
+    // Basically we are recreating ElementFinder again with same parameters
+    super(elementFinder.browser_, elementFinder.elementArrayFinder_);
+    this.name = name
   }
 
   async untilClickable() {
     let condition = until.elementToBeClickable(this);
     await browser.wait(condition);
   }
+
+  async untilPresent() {
+    let condition = until.presenceOf(this);
+    await browser.wait(condition);
+  }
+
+  async clickWhenReady() {
+    await this.untilPresent();
+    await this.untilClickable();
+    await this.click();
+    this.log('Clicked')
+  }
+
+  // add logging mixin
+  log: (action: string, ...msg: string[]) => void;
 }
 
+
+mixins.applyMixins(BaseElement, [mixins.Logging]);
