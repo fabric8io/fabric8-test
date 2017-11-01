bc31eb875ba0ae8e0a8c91fc4d34f98cd6ffe062
Sunil Thaha <sthaha@redhat.com>
2017-11-02 00:21:42 +1000

    asdf

--- ee_tests/src/specs/page_objects/ui/index.ts
+++ ee_tests/src/specs/page_objects/ui/index.ts
@@ -1,4 +1,4 @@
-export { BaseElement } from './base.element';
+export { BaseElementInterface, BaseElement } from './base.element';
 export { Button } from './button';
 export { Dropdown } from './dropdown';
 export { TextInput } from './text_input';
