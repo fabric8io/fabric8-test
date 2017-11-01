8af900f4e62ba9fc879d31777cb3562298e9f583
Sunil Thaha <sthaha@redhat.com>
2017-11-02 00:21:19 +1000

    BaseElement support waiting for text to appear

--- ee_tests/src/specs/page_objects/ui/base.element.ts
+++ ee_tests/src/specs/page_objects/ui/base.element.ts
@@ -1,8 +1,16 @@
 import { browser, ExpectedConditions as until, ElementFinder } from 'protractor';
-import { info } from '../../support';
+import { info, WAIT } from '../../support';
 import * as mixins from '../../mixins';
 
-export class BaseElement extends ElementFinder {
+export interface BaseElementInterface {
+  untilPresent(): Promise<any>;
+  untilClickable(): Promise<any>;
+  clickWhenReady(): Promise<any>;
+}
+
+
+export class BaseElement extends ElementFinder implements BaseElementInterface {
+
   // add logging mixin
   name: string = '';
   log: (action: string, ...msg: string[]) => void;
@@ -13,20 +21,20 @@ export class BaseElement extends ElementFinder {
    * @param {ElementFinder} elementFinder ElementFinder that you want to extend
    * @param {string} name to indentify the element in the logs
    */
-  constructor(elementFinder: ElementFinder, name: string = '') {
+  constructor(elementFinder: ElementFinder, name?: string) {
     // Basically we are recreating ElementFinder again with same parameters
     super(elementFinder.browser_, elementFinder.elementArrayFinder_);
-    this.name = name;
+    this.name = name || 'unnamed';
   }
 
-  async untilClickable() {
+  async untilClickable(wait: number = WAIT) {
     let condition = until.elementToBeClickable(this);
-    await browser.wait(condition);
+    await browser.wait(condition, wait);
   }
 
-  async untilPresent() {
+  async untilPresent(wait: number = WAIT) {
     let condition = until.presenceOf(this);
-    await browser.wait(condition);
+    await browser.wait(condition, wait);
   }
 
   async clickWhenReady() {
@@ -34,6 +42,11 @@ export class BaseElement extends ElementFinder {
     await this.untilClickable();
     await this.click();
     this.log('Clicked');
+  }
+
+  async untilTextIsPresent(text: string) {
+    let condition = until.textToBePresentInElement(this, text )
+    await browser.wait(condition)
   }
 
 }
