de8d88df7eea9a5c5292ad1ef93c3d5adf944667
Sunil Thaha <sthaha@redhat.com>
2017-11-02 00:21:58 +1000

    ModalBox

--- ee_tests/src/specs/page_objects/ui/index.ts
+++ ee_tests/src/specs/page_objects/ui/index.ts
@@ -2,3 +2,4 @@ export { BaseElementInterface, BaseElement } from './base.element';
 export { Button } from './button';
 export { Dropdown } from './dropdown';
 export { TextInput } from './text_input';
+export { ModalDialog } from './modal_dialog';
