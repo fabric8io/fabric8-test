a64cdce819a3f487b86b93162b30cf3eb11526b8
Sunil Thaha <sthaha@redhat.com>
2017-11-02 21:35:41 +1000

    Add functional tests for testing Page.open

--- ee_tests/protractorTS.config.ts
+++ ee_tests/protractorTS.config.ts
@@ -21,40 +21,14 @@ let conf: Config = {
     useAllAngular2AppRoots: true,
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['src/tests/**/EE/*.js'],
-    exclude: ['src/tests/**/EXCLUDE/*.js'],
+    specs: [
+      'src/specs/*.js',
+      'src/specs/**/*.js',
+    ],
 
     suites: {
-      allTest: ['src/tests/**/EE/*burr*.spec.js'],
-      analyticTest: ['src/tests/**/EE/*burr*analytic*.spec.js'],
-      cheTest: ['src/tests/**/EE/*burr-che-quickstart.spec.js'],
-      chequickstartTest: ['src/tests/**/EE/*burr-che.spec.js'],
-      importTest: ['src/tests/**/EE/*burr*import*.spec.js'],
-      mainTest: [
-        'src/tests/**/EE/*burr*che.spec.js',
-        'src/tests/**/EE/*burr*pipeline.spec.js',
-        'src/tests/**/EE/*burr*quickstart.spec.js'
-      ],
-      quickstartTest: ['src/tests/**/EE/*burr-quickstart.spec.js'],
-      pipelineTest: ['src/tests/**/EE/*burr*pipeline*.spec.js'],
-      runTest: ['src/tests/**/EE/*burr*analytic*.spec.js'],
-      setupTest: ['src/tests/**/EE/*setup*.spec.js'],
-      terminalTest: ['src/tests/**/EE/*burr*terminal*.spec.js'],
-
-      justChe: [
-        'src/tests/**/EE/*burr-che.spec.js',
-        'src/tests/**/EE/*burr-import-che.spec.js',
-        'src/tests/**/EE/*burr-che-quickstart.spec.js'
-      ],
-
-      justJenkins: [
-        'src/tests/**/EE/*burr-pipeline.spec.js',
-        'src/tests/**/EE/*burr-import-pipeline.spec.js',
-        'src/tests/**/EE/*burr-quickstart.spec.js'
-      ],
-
-      tempTest: ['src/tests/**/EE/*burr-import-pipeline.spec.js'],
-      specs: ['src/specs/**/*.spec.js']   // new typescript based specs
+      specs: ['src/specs/**/*.spec.js'],   // new typescript based specs
+      functional: ['src/functional/**/*.spec.js']   // new typescript based specs
     },
 
     // see: https://github.com/angular/protractor/blob/master/docs/timeouts.md
