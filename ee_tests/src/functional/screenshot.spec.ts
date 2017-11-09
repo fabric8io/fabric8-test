c7699bd1e179f68f0210ddbe33de295544f6126b
Sunil Thaha <sthaha@redhat.com>
2017-11-09 16:25:06 +1000

    Add functional test for writeScreenshot

---
+++ ee_tests/src/functional/screenshot.spec.ts
@@ -0,0 +1,19 @@
+import { browser } from 'protractor';
+import * as support from '../specs/support';
+import { LandingPage } from '../specs/page_objects';
+
+describe('Landing Page', () => {
+
+  beforeEach( async () => {
+    await support.desktopTestSetup();
+
+    let landingPage = new LandingPage();
+    await landingPage.open();
+  });
+
+  fit('writes the screenshot of landing page', async () => {
+    await expect(await browser.getTitle()).toEqual('OpenShift.io');
+    await support.writeScreenshot('page.png');
+  });
+
+});
