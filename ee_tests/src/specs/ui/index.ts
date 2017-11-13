8b751a5f71d288841507e51fb16333382ee74d9e
Sunil Thaha <sthaha@redhat.com>
2017-11-13 11:17:34 +1000

    ui: export everything

--- ee_tests/src/specs/ui/index.ts
+++ ee_tests/src/specs/ui/index.ts
@@ -1,5 +1,5 @@
 export * from './base.element';
-export { Button } from './button';
-export { Dropdown } from './dropdown';
-export { TextInput } from './text_input';
-export { ModalDialog } from './modal_dialog';
+export * from './button';
+export * from './dropdown';
+export * from './text_input';
+export * from './modal_dialog';
