fbe2f52f6e038a64b2a996f06f428cd30e0f0dd8
Sunil Thaha <sthaha@redhat.com>
2017-11-13 15:59:51 +1000

    Export everything from space-dashboard page

--- ee_tests/src/specs/page_objects/index.ts
+++ ee_tests/src/specs/page_objects/index.ts
@@ -1,7 +1,8 @@
 export { PageOpenMode, BasePage } from './base.page';
 export { LandingPage } from './landing.page';
 export { MainDashboardPage } from './main_dashboard.page';
-export { SpaceDashboardPage } from './space_dashboard.page';
+export * from './space_dashboard.page';
+export * from './space_dashboard/add_to_space_dialog';
 
 
 // TODO: move page to a different file
