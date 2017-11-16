aef3834fccfdee1add138bdfd30fb8a3e8a16e14
Sunil Thaha <sthaha@redhat.com>
2017-11-16 18:14:22 +1000

    Create PageObjects for Planner Tab in the Dashboard
    The SpaceDashboard is refactored to represent its current form which
    is that there are 3 Tabs in the "Main Navigation Bar"
    
    The inheritance is as follows
      AppPage
      └── SpaceTabPage
          ├── PlannerPage
          └── SpaceDashboardPage
    
    The base page for all Tabs is the SpaceTabPage and has methods to
    navigate to other tabs

--- ee_tests/src/specs/page_objects/space_dashboard.page.ts
+++ ee_tests/src/specs/page_objects/space_dashboard.page.ts
@@ -8,7 +8,7 @@
 
 import { browser, element, by, By, ExpectedConditions as until, $, $$, ElementFinder } from 'protractor';
 import { AppPage } from './app.page';
-import { TextInput, Button } from '../ui';
+import * as ui from '../ui';
 import { AddToSpaceDialog } from './space_dashboard/add_to_space_dialog'
 
 /*
@@ -34,10 +34,119 @@ Page layout
 |--------------------------------------------------------------------------------------------------------------------|
 */
 
-/* Dialog to create new space and project */
 
-export class SpaceDashboardPage extends AppPage {
+abstract class SpaceTabPage extends AppPage {
 
+  mainNavBar = new ui.BaseElement(
+    this.header.$('ul.nav.navbar-nav.navbar-primary.persistent-secondary'),
+    'Main Navigation Bar'
+
+  )
+
+  planTab = new ui.Clickable(
+    this.mainNavBar.element(by.cssContainingText('li', 'Plan')),
+    'Plan'
+  );
+
+  constructor(public spaceName: string) {
+    super();
+  }
+
+  // todo: add ready when we can consider the headers ready
+  async ready() {
+    await super.ready()
+    await this.mainNavBar.ready()
+    await this.planTab.ready()
+  }
+
+  async gotoPlanTab(): Promise<PlannerTab> {
+    await this.planTab.clickWhenReady()
+    // NOTE: outside the dialog is outside of $(this)
+    let planner  = new PlannerTab(this.spaceName)
+    await planner.open();
+    return planner;
+  }
+
+}
+
+type WorkItemType = 'task' | 'feature' | 'bug'
+
+interface WorkItem {
+  title: string
+  description?: string
+  type?: WorkItemType
+}
+
+class WorkItemQuickAdd extends ui.Clickable {
+  titleTextInput = new ui.TextInput(this.$('input.f8-quickadd-input'), 'Work item Title')
+  buttonsDiv = this.$('div.f8-quickadd__wiblk-btn.pull-right')
+  acceptButton = new ui.Button(this.buttonsDiv.$('button.btn.btn-primary'), '✓')
+  cancelButton = new ui.Button(this.buttonsDiv.$('button.btn.btn-default'), 'x')
+
+  constructor(el: ElementFinder, name = 'Work Item Quick Add') {
+    super(el, name)
+  }
+
+  async ready() {
+    await super.ready()
+    await this.untilClickable()
+  }
+
+  async createWorkItem({ title, description = '', type = 'feature' }: WorkItem) {
+    await this.clickWhenReady();
+    await this.titleTextInput.ready()
+    await this.titleTextInput.enterText(title)
+    await this.cancelButton.untilClickable();
+
+    await this.acceptButton.clickWhenReady()
+
+    // TODO add more confirmation that the item has been added
+    await this.cancelButton.clickWhenReady()
+
+    // TODO choose the type of item
+    this.log('New WorkItem', `${title} added`)
+  }
+}
+
+class WorkItemList extends ui.BaseElement {
+  overlay = new ui.BaseElement(this.$('div.lock-overlay-list'));
+
+  quickAdd =  new WorkItemQuickAdd(
+    this.$('#workItemList_quickAdd > alm-work-item-quick-add > div'));
+
+  constructor(el: ElementFinder, name = 'Work Item List') {
+    super(el, name)
+  }
+
+  async ready() {
+    await super.ready()
+    await this.overlay.untilAbsent()
+    await this.quickAdd.ready()
+  }
+}
+
+// this is what you see when you click on the Plan Tab button
+class PlannerTab extends SpaceTabPage {
+  workItemList = new WorkItemList(this.appTag.$('alm-work-item-list'));
+
+  constructor(public spaceName: string) {
+    super(spaceName);
+    this.url = `${browser.params.login.user}/${spaceName}/plan`
+  }
+
+  async ready() {
+    await super.ready();
+    await this.workItemList.ready();
+  }
+
+  async createWorkItem(item: WorkItem) {
+    this.debug('create item', JSON.stringify(item))
+    await this.workItemList.quickAdd.createWorkItem(item)
+  }
+}
+
+// The main page that represents a Space
+export class SpaceDashboardPage extends SpaceTabPage {
 
  /* Dialog to create new space and project */
   newSpaceName = $('#name');
@@ -106,7 +215,7 @@ export class SpaceDashboardPage extends AppPage {
   /* Pipelines section title/link */
   pipelinesSectionTitle = $('#spacehome-pipelines-title');
   addToSpaceButton = this.innerElement(
-    Button, '#analyze-overview-add-to-space-button', 'Add to space')
+    ui.Button, '#analyze-overview-add-to-space-button', 'Add to space')
 
   /* UI Page Section: Environments */
   environments = $('spacehome-environments-card');
@@ -132,8 +241,8 @@ export class SpaceDashboardPage extends AppPage {
   spaceName: string;
 
   constructor(spaceName: string) {
-    super();
-    this.spaceName = spaceName;
+    super(spaceName);
+
     // TODO: create a better way to access globals like username
     this.url = `${browser.params.login.user}/${spaceName}`
   }
@@ -151,4 +260,5 @@ export class SpaceDashboardPage extends AppPage {
     await wizard.open();
     return wizard;
   }
+
 }
