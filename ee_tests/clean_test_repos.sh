c21ede712a03b94629b95882e4259459fc1af2f4
ldimaggi <ldimaggi@redhat.com>
2017-11-30 08:00:31 -0500

    fix(test): this change is needed to restore centos CI jobs

--- ee_tests/clean_test_repos.sh
+++ ee_tests/clean_test_repos.sh
@@ -2,7 +2,7 @@
 # $2 = github username
 set -x
 
-REPOS=`curl -X  GET https://api.github.com/users/$2/repos -H "Authorization: token $1" | grep \"name\" | sed 's/\"name\": \"//g' | sed s'/ //g' | sed s'/",//g' | grep -v vertxbasic`
+REPOS=`curl -X  GET https://api.github.com/users/$2/repos -H "Authorization: token $1" | grep \"name\" | sed 's/\"name\": \"//g' | sed s'/ //g' | sed s'/",//g' | grep -v vertxbasic | grep -v ApacheLicense2.0`
 
 for repo in $REPOS; do echo "deleting repo " $repo ; curl -X DELETE -H "Authorization: token $1" https://api.github.com/repos/$2/$repo; done
 
