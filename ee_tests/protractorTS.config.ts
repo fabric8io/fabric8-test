90e189aeb560fe52175fc3ba12e83543fa3f3855
ldimaggi <ldimaggi@redhat.com>
2017-12-22 18:57:15 -0500

    fix(test) increase jasmine timwou as Che is slow to start

--- ee_tests/protractorTS.config.ts
+++ ee_tests/protractorTS.config.ts
@@ -24,7 +24,7 @@ let conf: Config = {
       showColors: true,
       silent: true,
       isVerbose: true,
-      defaultTimeoutInterval: 10 * 60 * 1000 // 10 mins for spec to run
+      defaultTimeoutInterval: 15 * 60 * 1000 // 15 mins for spec to run
     },
 
     directConnect: process.env.DIRECT_CONNECTION === 'true',
