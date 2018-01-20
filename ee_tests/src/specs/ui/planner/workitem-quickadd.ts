707ffbd690d8d16c13dba1ca646956b6517a8717
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-01-20 16:45:07 +0530

    Add notification toast to workitem quickadd
    - Wait for notification toast to vanish before returning from addWorkItem function

--- ee_tests/src/specs/ui/planner/workitem-quickadd.ts
+++ ee_tests/src/specs/ui/planner/workitem-quickadd.ts
@@ -1,12 +1,15 @@
-import { ElementFinder } from 'protractor';
+import { ElementFinder, $ } from 'protractor';
 import { WorkItem } from './index';
 import * as ui from '../../ui';
 
-export class WorkItemQuickAdd extends ui.Clickable {
+export class WorkItemQuickAdd extends ui.BaseElement {
   titleTextInput = new ui.TextInput(this.$('input.f8-quickadd-input'), 'Work item Title');
   buttonsDiv = this.$('div.f8-quickadd__wiblk-btn.pull-right');
-  addButton = new ui.Button(this.buttonsDiv.$('button.btn.btn-primary'), 'Add Button');
-  addAndOpenButton = new ui.Button(this.buttonsDiv.$('button.btn.btn-default'), 'Add and Open Button');
+  addButton = new ui.Button(this.buttonsDiv.$$('button.btn.btn-primary').first(), 'Add Button');
+  addAndOpenButton = new ui.Button(this.buttonsDiv.$$('button.btn.btn-primary').last(), 'Add and Open Button');
+
+  // TODO - make notification seperate class and in mixin
+  notificationToast = new ui.BaseElement($('div.toast-notifications-list-pf'), 'Notification Toast');
 
   constructor(el: ElementFinder, name = 'Work Item Quick Add') {
     super(el, name);
@@ -14,7 +17,7 @@ export class WorkItemQuickAdd extends ui.Clickable {
 
   async ready() {
     await super.ready();
-    await this.untilClickable();
+    await this.addAndOpenButton.ready();
   }
 
   async addWorkItem({ title, description = '', type = 'feature' }: WorkItem) {
@@ -22,8 +25,10 @@ export class WorkItemQuickAdd extends ui.Clickable {
     await this.titleTextInput.ready();
     await this.titleTextInput.enterText(title);
     await this.addAndOpenButton.untilClickable();
-
     await this.addButton.clickWhenReady();
+    // TODO - Remove following 2 lines when notification is moved to a seperate class
+    await this.notificationToast.untilPresent();
+    await this.notificationToast.untilAbsent();
 
     // TODO add more confirmation that the item has been added
     this.log('New WorkItem', `${title} added`);
