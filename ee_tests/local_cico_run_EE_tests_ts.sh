8ed4fc316e632ed8bf020149c85fd90f08352d18
Lucia Jelinkova <ljelinko@redhat.com>
2018-02-07 10:10:06 +0100

    refactor(scripts) Refactored docker scripts
    Refactored docker scripts and created local script for building and
    running local tests in docker.
    
    npm run image:build
    npm run image:start

---
+++ ee_tests/local_cico_run_EE_tests_ts.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Export test configuration
+. ./config/local_osio.conf.sh
+
+# Show command before executing
+set -x
+
+# Stop on error
+set -e
+
+mkdir -p dist
+
+# Shutdown container if running
+if [ -n "$(docker ps -q -f name=fabric8-test)" ]; then
+    docker rm -f fabric8-test
+fi
+
+# Run and setup Docker image 
+docker run --detach=true --name=fabric8-test --cap-add=SYS_ADMIN \
+          -e OSIO_USERNAME -e OSIO_PASSWORD -e OSIO_URL  \
+          -e OSO_USERNAME -e GITHUB_USERNAME -e TEST_SUITE -e QUICKSTART_NAME -e RELEASE_STRATEGY -e DEBUG -e "API_URL=http://api.openshift.io/api/" \
+          -t -v $(pwd)/dist:/dist:Z fabric8-test:latest
+
+# Exec EE tests
+docker exec fabric8-test ./ts-protractor.sh $TEST_SUITE | tee target/theLog.txt
+
+# Test results to archive
+docker cp fabric8-test:/opt/fabric8-test/target/. target
+
+# Shutdown container if running
+if [ -n "$(docker ps -q -f name=fabric8-test)" ]; then
+    docker rm -f fabric8-test
+fi
+
+
+
+
+
