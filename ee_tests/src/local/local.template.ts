e17979007f2c54f6b2b040179b156674368d3a4c
Lucia Jelinkova <ljelinko@redhat.com>
2018-03-07 12:47:46 +0100

    Added support for testing against local page

---
+++ ee_tests/src/local/local.template.ts
@@ -0,0 +1,33 @@
+import { browser, element, by, ExpectedConditions as until, $, $$ } from 'protractor';
+import { WebDriver, error as SE } from 'selenium-webdriver';
+
+import * as support from '../support';
+import { Quickstart } from '../support/quickstart';
+import { LandingPage } from '../page_objects/landing.page';
+import { SpaceDashboardPage } from '../page_objects/space_dashboard.page';
+import { SpacePipelinePage } from '../page_objects/space_pipeline.page';
+import { MainDashboardPage } from '../page_objects/main_dashboard.page';
+import { StageRunPage } from '../page_objects/space_stage_run.page';
+import { SpaceDeploymentsPage } from '../page_objects/space_deployments.page';
+
+/**
+ * Test that can run locally agains static page stored on filesystem
+ */
+describe('Creating new quickstart in OSIO', () => {
+
+  // URL of local file that sould be loaded in browser
+  let URL: string = '';
+
+  beforeEach(async () => {
+    browser.resetUrl = 'file://';
+    browser.ignoreSynchronization = true;
+    await browser.get(URL);
+  });
+
+  afterEach(async () => {
+  });
+
+  it('Local test', async () => {
+    // put testing code here
+  });
+});
