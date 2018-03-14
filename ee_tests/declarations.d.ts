de054884f499505720e18caaf92ffac723c69ace
Lucia Jelinkova <ljelinko@redhat.com>
2018-03-14 09:10:17 +0100

    Added protractor-fail-fast
    Added protractor-fail-fast dependency that allows to exit tests  on the first failure
    instead of running all tests.

--- ee_tests/declarations.d.ts
+++ ee_tests/declarations.d.ts
@@ -2,3 +2,4 @@ declare module "HtmlScreenshotReporter";
 declare module "protractor-jasmine2-screenshot-reporter";
 declare module "jasmine-spec-reporter";
 declare module "jasmine-protractor-matchers";
+declare module 'protractor-fail-fast';
