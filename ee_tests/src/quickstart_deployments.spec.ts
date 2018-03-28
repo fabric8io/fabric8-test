4eb301386e9bba9e3c5e8e0f91cba570b583cb5f
Lucia Jelinkova <ljelinko@redhat.com>
2018-03-28 08:56:56 +0200

    Separated tests to it() methods

--- ee_tests/src/quickstart_deployments.spec.ts
+++ ee_tests/src/quickstart_deployments.spec.ts
@@ -1,4 +1,4 @@
-import { browser, element, by, ExpectedConditions as until, $, $$ } from 'protractor';
+import { browser, element, by, ExpectedConditions as until, $, $$, ProtractorBrowser } from 'protractor';
 import { WebDriver, error as SE } from 'selenium-webdriver';
 
 import * as support from './support';
@@ -6,63 +6,65 @@ import { Quickstart } from './support/quickstart';
 // tslint:disable-next-line:max-line-length
 import { DeploymentsInteractions } from './interactions/deployments_interactions';
 import { PipelinesInteractions } from './interactions/pipelines_interactions';
-import { TextInput, Button } from './ui';
-
 import { LandingPage } from './page_objects/landing.page';
 import { SpaceDashboardPage } from './page_objects/space_dashboard.page';
 import { SpacePipelinePage } from './page_objects/space_pipeline.page';
 import { MainDashboardPage } from './page_objects/main_dashboard.page';
-import { StageRunPage } from './page_objects/space_stage_run.page';
 // tslint:disable-next-line:max-line-length
 import { SpaceDeploymentsPage, DeploymentStatus, DeployedApplication, DeployedApplicationEnvironment, Environment} from './page_objects/space_deployments.page';
-import { spawn } from 'child_process';
 
-let globalSpaceName: string;
-let globalSpacePipelinePage: SpacePipelinePage;
-
-/* Tests to verify the build pipeline */
-
-describe('Creating new quickstart in OSIO', () => {
+describe('Main E2E test suite', () => {
   let dashboardPage: MainDashboardPage;
+  let spaceDashboardPage: SpaceDashboardPage;
+  let quickstart: Quickstart;
+  let strategy: string;
+  let spaceName: string;
+  let index: number = 1;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    support.info('--- Before all ---');
     await support.desktopTestSetup();
+    spaceName = support.newSpaceName();
+    strategy = browser.params.release.strategy;
+    quickstart = new Quickstart(browser.params.quickstart.name);
+  });
+
+  afterEach(async () => {
+    support.info('--- After each ---');
+    support.writeScreenshot('target/screenshots/' + spaceName + '_' + index + '.png');
+    support.writePageSource('target/screenshots/' + spaceName + '_' + index + '.html');
+    index++;
+  });
+
+  it('Login', async () => {
+    support.info('--- Login ---');
     let login = new support.LoginInteraction();
     dashboardPage = await login.run();
   });
 
-  afterEach(async () => {
-    await browser.sleep(support.DEFAULT_WAIT);
-    // await support.dumpLog2(globalSpacePipelinePage, globalSpaceName);
-    support.writeScreenshot('target/screenshots/pipeline_final_' + globalSpaceName + '.png');
-    support.writePageSource('target/screenshots/pipeline_final_' + globalSpaceName + '.html');
-    // support.info('\n ============ End of test reached, logging out ============ \n');
-    // await dashboardPage.logout();
+  it('Create space ', async () => {
+    support.info('--- Create space ' + spaceName + ' ---');
+    spaceDashboardPage = await dashboardPage.createNewSpace(spaceName);
   });
 
-  it('Create a new space, new ' + browser.params.quickstart.name + ' quickstart, run its pipeline', async () => {
-    let quickstart = new Quickstart(browser.params.quickstart.name);
-    let spaceName = support.newSpaceName();
-    globalSpaceName = spaceName;
-    let spaceDashboardPage = await dashboardPage.createNewSpace(spaceName);
-
+  it('Create quickstart', async () => {
+    support.info('--- Create quickstart ' + quickstart.name + ' ---');
     let wizard = await spaceDashboardPage.addToSpace();
-
-    //    let strategy: string  = 'releaseStageApproveAndPromote';
-    let strategy: string = browser.params.release.strategy;   // 'release';
-
-    support.info('Creating quickstart: ' + quickstart.name);
     await wizard.newQuickstartProject({ project: quickstart.name, strategy });
     await spaceDashboardPage.ready();
+  });
 
+  it('Run pipeline', async () => {
+    support.info('--- Run pipeline ---');
     await browser.sleep(5000);
 
     let pipelineInteractions = PipelinesInteractions.create(strategy, spaceName, spaceDashboardPage);
     await pipelineInteractions.showPipelinesScreen();
     await pipelineInteractions.waitToFinish();
+  });
 
-    support.writeScreenshot('target/screenshots/pipeline_icons_' + spaceName + '.png');
-
+  it('Verify deployment', async () => {
+    support.info('--- Verify deployments ---');
     await browser.sleep(5000);
 
     // tslint:disable-next-line:max-line-length
@@ -71,8 +73,5 @@ describe('Creating new quickstart in OSIO', () => {
     let application = await deploymentsInteractions.verifyApplication();
     await deploymentsInteractions.verifyEnvironments(application);
     await deploymentsInteractions.verifyResourceUsage();
-
-    support.writeScreenshot('target/screenshots/pipeline_deployments_' + spaceName + '.png');
-    support.writePageSource('target/screenshots/pipeline_deployments_' + spaceName + '.html');
   });
 });
