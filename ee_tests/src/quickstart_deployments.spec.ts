807ade41120ffd14b5392677984a83785baac0ac
Lucia Jelinkova <ljelinko@redhat.com>
2018-04-05 15:21:16 +0200

    Merged che and dashboard tests into smoketest
    Related to #572 and #573

--- ee_tests/src/quickstart_deployments.spec.ts
+++ ee_tests/src/quickstart_deployments.spec.ts
@@ -3,15 +3,16 @@ import { WebDriver, error as SE } from 'selenium-webdriver';
 
 import * as support from './support';
 import { Quickstart } from './support/quickstart';
-// tslint:disable-next-line:max-line-length
 import { DeploymentsInteractions } from './interactions/deployments_interactions';
 import { PipelinesInteractions } from './interactions/pipelines_interactions';
 import { LandingPage } from './page_objects/landing.page';
 import { SpaceDashboardPage } from './page_objects/space_dashboard.page';
 import { SpacePipelinePage } from './page_objects/space_pipeline.page';
 import { MainDashboardPage } from './page_objects/main_dashboard.page';
-// tslint:disable-next-line:max-line-length
-import { SpaceDeploymentsPage, DeploymentStatus, DeployedApplication, DeployedApplicationEnvironment, Environment} from './page_objects/space_deployments.page';
+import { SpaceChePage } from './page_objects/space_che.page';
+import { SpaceCheWorkspacePage } from './page_objects/space_cheworkspace.page';
+import { Button } from './ui';
+import { BuildStatus } from '..';
 
 describe('Main E2E test suite', () => {
   let dashboardPage: MainDashboardPage;
@@ -54,8 +55,48 @@ describe('Main E2E test suite', () => {
     await spaceDashboardPage.ready();
   });
 
+  it('Run che', async () => {
+    support.info('--- Run che workspace ' + quickstart.name + ' ---');
+    await spaceDashboardPage.codebasesSectionTitle.clickWhenReady();
+
+    let spaceChePage = new SpaceChePage();
+    await spaceChePage.createCodebase.clickWhenReady(support.LONGEST_WAIT);
+
+    /* A new browser window is opened when Che opens - switch to that new window now */
+    let handles = await browser.getAllWindowHandles();
+    support.debug('Number of browser tabs before = ' + handles.length);
+
+    /* TODO - Need to create a query to look for/wait for the 2nd browser window and remove the sleep statement */
+    await browser.sleep(60000);
+    handles = await browser.getAllWindowHandles();
+    support.debug('Number of browser tabs after = ' + handles.length);
+    support.writeScreenshot('target/screenshots/che_workspace_parta_' + spaceName + '.png');
+
+    /* Switch to the Che browser window */
+    await browser.switchTo().window(handles[1]);
+
+    let spaceCheWorkSpacePage = new SpaceCheWorkspacePage();
+    support.writeScreenshot('target/screenshots/che_workspace_partb_' + spaceName + '.png');
+
+    let projectInCheTree = new Button(spaceCheWorkSpacePage.recentProjectRootByName(spaceName), 'Project in Che Tree');
+    await projectInCheTree.untilPresent(support.LONGEST_WAIT);
+    // await support.debug (spaceCheWorkSpacePage.recentProjectRootByName(spaceName).getText());
+    support.writeScreenshot('target/screenshots/che_workspace_partc_' + spaceName + '.png');
+
+    expect(await spaceCheWorkSpacePage.recentProjectRootByName(spaceName).getText()).toContain(spaceName);
+
+    /* Switch back to the OSIO browser window */
+    await browser.switchTo().window(handles[0]);
+  });
+
   it('Run pipeline', async () => {
     support.info('--- Run pipeline ---');
+    await browser.sleep(5000);
+    await dashboardPage.header.recentItemsDropdown.clickWhenReady();
+    await dashboardPage.header.recentItemsDropdown.accountHomeItem.clickWhenReady();
+    await dashboardPage.header.recentItemsDropdown.clickWhenReady();
+    await dashboardPage.recentSpaceByName(spaceName).click();
+    await dashboardPage.ready();
     await browser.sleep(5000);
 
     let pipelineInteractions = PipelinesInteractions.create(strategy, spaceName, spaceDashboardPage);
@@ -75,5 +116,54 @@ describe('Main E2E test suite', () => {
     let application = await deploymentsInteractions.verifyApplication();
     await deploymentsInteractions.verifyEnvironments(application);
     await deploymentsInteractions.verifyResourceUsage();
+  });
+
+  it('Verify dashboard', async () => {
+    support.info('--- Verify dashboard ---');
+
+    await dashboardPage.header.recentItemsDropdown.clickWhenReady();
+    await dashboardPage.header.recentItemsDropdown.accountHomeItem.clickWhenReady();
+    await dashboardPage.header.recentItemsDropdown.clickWhenReady();
+    await dashboardPage.recentSpaceByName(spaceName).click();
+    await dashboardPage.ready();
+
+    let codebasesCard = await spaceDashboardPage.getCodebaseCard();
+    expect(await codebasesCard.getCount()).toBe(1, 'number of codebases on page');
+
+    let githubName = browser.params.github.username;
+    let codebases = await codebasesCard.getCodebases();
+    expect(codebases.length).toBe(1, 'number of codebases');
+    expect(codebases[0]).toBe('https://github.com/' + githubName + '/' + spaceName);
+
+    let workItemsCard = await spaceDashboardPage.getWorkItemsCard();
+    expect(await workItemsCard.getCount()).toBe(0, 'number of workitems on page');
+
+    let pipelinesCard = await spaceDashboardPage.getPipelinesCard();
+    expect(await pipelinesCard.getCount()).toBe(1, 'number of pipelines on page');
+
+    let pipelines = await pipelinesCard.getPipelines();
+    expect(pipelines.length).toBe(1, 'number of pipelines');
+    expect(pipelines[0].getApplication()).toBe(spaceName, 'application name on pipeline');
+    expect(pipelines[0].getStatus()).toBe(BuildStatus.COMPLETE, 'build status');
+    expect(pipelines[0].getBuildNumber()).toBe(1, 'build number');
+
+    let deploymentsCard = await spaceDashboardPage.getDeploymentsCard();
+    // expect(await deploymentsCard.getCount()).toBe(1, 'number of deployments on page');
+
+    let applications = await deploymentsCard.getApplications();
+    expect(applications.length).toBe(1, 'number of applications');
+    expect(await applications[0].getName()).toBe(spaceName, 'deployed application name');
+    // expect(await applications[0].getStageVersion()).toBe('1.0.1', 'deployed application stage version');
+    // expect(await applications[0].getRunVersion()).toBe('1.0.1', 'deployed application run version');
+
+    let analyticsCard = await spaceDashboardPage.getAnalyticsCard();
+    let totalCount = await analyticsCard.getTotalDependenciesCount();
+    let analyzedCount = await analyticsCard.getAnalyzedDependenciesCount();
+    let unknownCount = await analyticsCard.getUnknownDependenciesCount();
+
+    expect(totalCount).toBeGreaterThanOrEqual(0, 'total dependencies count');
+    expect(analyzedCount).toBeGreaterThanOrEqual(0, 'total analyzed count');
+    expect(unknownCount).toBeGreaterThanOrEqual(0, 'total unknown count');
+    expect(totalCount).toBe(analyzedCount + unknownCount, 'total = analyzed + unknown');
   });
 });
