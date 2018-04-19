05c03fba9b135214e153ade28f39352277990fce
Pavol Pitonak <ppitonak@redhat.com>
2018-04-19 13:46:55 +0200

    tag Docker builder image in cico_build_deploy.sh

--- ee_tests/cico_build_deploy.sh
+++ ee_tests/cico_build_deploy.sh
@@ -21,7 +21,6 @@ if [ -n "${BUILD_NUMBER}" ]; then
   service docker start
 fi
 
-
 IMAGE="fabric8-test"
 REPOSITORY="fabric8io"
 REGISTRY="push.registry.devshift.net"
@@ -29,27 +28,39 @@ REGISTRY="push.registry.devshift.net"
 # Build image
 docker build -t ${IMAGE} -f Dockerfile.builder .
 
-## All ok, deploy
-if [ $? -eq 0 ]; then
-  echo 'CICO: build OK'
+if [ ! $? -eq 0 ]; then
+  echo 'CICO: Image build failed'
+  exit 1
+fi
+echo 'CICO: Build OK'
 
-  if [ -z "${BUILD_NUMBER}" ]; then
-    exit 0
-  fi
-
-  if [ -n "${DEVSHIFT_USERNAME}" ] && [ -n "${DEVSHIFT_PASSWORD}" ]; then
-      docker login -u ${DEVSHIFT_USERNAME} -p ${DEVSHIFT_PASSWORD} ${REGISTRY}
-  fi
-
-  docker tag ${IMAGE} ${REGISTRY}/${REPOSITORY}/${IMAGE}:latest && \
-  docker push ${REGISTRY}/${REPOSITORY}/${IMAGE}:latest
-  if [ $? -eq 0 ]; then
-    echo 'CICO: image pushed, npmjs published, ready to update deployed app'
-    exit 0
-  else
-    echo 'CICO: Image push to registry failed'
-    exit 2
-  fi
+if [ -z "${BUILD_NUMBER}" ]; then
+  exit 0
 fi
 
+# returns something like "Google Chrome 66.0.3359.117"
+TMP=`docker run --rm fabric8-test google-chrome --version`
+# convert to array of words
+TMP=($TMP)
+if [ ! ${#TMP[*]} -eq 3 ]; then
+  echo "CICO: The output of command 'google-chrome --version' probably changed, update this script";
+  exit 2
+fi
+TAG=${TMP[2]}
 
+if [ -n "${DEVSHIFT_USERNAME}" ] && [ -n "${DEVSHIFT_PASSWORD}" ]; then
+  docker login -u ${DEVSHIFT_USERNAME} -p ${DEVSHIFT_PASSWORD} ${REGISTRY}
+fi
+
+docker tag ${IMAGE} ${REGISTRY}/${REPOSITORY}/${IMAGE}:latest && \
+docker tag ${IMAGE} ${REGISTRY}/${REPOSITORY}/${IMAGE}:${TAG} && \
+docker push ${REGISTRY}/${REPOSITORY}/${IMAGE}:latest && \
+docker push ${REGISTRY}/${REPOSITORY}/${IMAGE}:${TAG}
+
+if [ $? -eq 0 ]; then
+  echo 'CICO: Image pushed, ready to update deployed app'
+  exit 0
+else
+  echo 'CICO: Image push to registry failed'
+  exit 3
+fi
