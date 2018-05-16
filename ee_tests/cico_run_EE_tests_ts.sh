5ba0c84f75cc7eca6b6d3c57192618fa578a47fc
Lucia Jelinkova <ljelinko@redhat.com>
2018-05-16 09:43:19 +0200

    Created unique name for container

--- ee_tests/cico_run_EE_tests_ts.sh
+++ ee_tests/cico_run_EE_tests_ts.sh
@@ -88,12 +88,14 @@ export ZABBIX_ENABLED=$ZABBIX_ENABLED
 export ZABBIX_HOST=$ZABBIX_HOST
 export ZABBIX_METRIC_PREFIX=$FEATURE_LEVEL
 
+export CONTAINER_NAME = $JOB_NAME;
+
 # Shutdown container if running
-if [ -n "$(docker ps -q -f name=fabric8-test)" ]; then
-    docker rm -f fabric8-test
+if [ -n "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    docker rm -f $CONTAINER_NAME
 fi
 
-docker run --shm-size=256m --detach=true --name=fabric8-test --cap-add=SYS_ADMIN \
+docker run --shm-size=256m --detach=true --name=$CONTAINER_NAME --cap-add=SYS_ADMIN \
           -e OSIO_USERNAME -e OSIO_PASSWORD -e OSIO_URL -e OSO_USERNAME -e GITHUB_USERNAME \
           -e TEST_SUITE -e QUICKSTART_NAME -e RELEASE_STRATEGY -e FEATURE_LEVEL -e DEBUG -e "API_URL=http://api.openshift.io/api/" \
           -e ARTIFACT_PASSWORD=$ARTIFACT_PASS -e BUILD_NUMBER -e JOB_NAME -e RESET_ENVIRONMENT \
@@ -102,10 +104,10 @@ docker run --shm-size=256m --detach=true --name=fabric8-test --cap-add=SYS_ADMIN
           -v $PWD/jenkins-env:/opt/fabric8-test/jenkins-env ${REGISTRY}/${REPOSITORY}/${IMAGE}:latest
 
 # Start Xvfb
-docker exec fabric8-test /usr/bin/Xvfb :99 -screen 0 1024x768x24 &
+docker exec $CONTAINER_NAME /usr/bin/Xvfb :99 -screen 0 1024x768x24 &
 
 # Exec EE tests
-docker exec fabric8-test ./ts-protractor.sh $TEST_SUITE | tee theLog.txt
+docker exec $CONTAINER_NAME ./ts-protractor.sh $TEST_SUITE | tee theLog.txt
 
 # Writing to and the grepping results required as webdriver fails
 # intermittently - which results is failure reported even if tests pass
@@ -123,20 +125,20 @@ if [ $ret1 -eq 1 -a $ret2 -eq 0 ]; then RTN_CODE=0; else RTN_CODE=1; fi
 ### RTN_CODE=$?
 
 # Archive test results file
-docker exec fabric8-test chmod 600 password_file
-docker exec fabric8-test chown root password_file
-docker exec fabric8-test ls -l password_file
-docker exec fabric8-test ls -l ./target/screenshots
-docker exec fabric8-test ls -l ./target/zabbix
+docker exec $CONTAINER_NAME chmod 600 password_file
+docker exec $CONTAINER_NAME chown root password_file
+docker exec $CONTAINER_NAME ls -l password_file
+docker exec $CONTAINER_NAME ls -l ./target/screenshots
+docker exec $CONTAINER_NAME ls -l ./target/zabbix
 
-docker exec fabric8-test mkdir -p ./e2e/${JOB_NAME}/${BUILD_NUMBER}
-docker exec fabric8-test bash -c 'cp ./target/screenshots/* ./e2e/${JOB_NAME}/${BUILD_NUMBER}'
-docker exec fabric8-test bash -c 'cp ./target/zabbix/* ./e2e/${JOB_NAME}/${BUILD_NUMBER}'
-docker exec fabric8-test rsync --password-file=./password_file -PHva --relative ./e2e/${JOB_NAME}/${BUILD_NUMBER}  devtools@artifacts.ci.centos.org::devtools/
+docker exec $CONTAINER_NAME mkdir -p ./e2e/${JOB_NAME}/${BUILD_NUMBER}
+docker exec $CONTAINER_NAME bash -c 'cp ./target/screenshots/* ./e2e/${JOB_NAME}/${BUILD_NUMBER}'
+docker exec $CONTAINER_NAME bash -c 'cp ./target/zabbix/* ./e2e/${JOB_NAME}/${BUILD_NUMBER}'
+docker exec $CONTAINER_NAME rsync --password-file=./password_file -PHva --relative ./e2e/${JOB_NAME}/${BUILD_NUMBER}  devtools@artifacts.ci.centos.org::devtools/
 
 # Shutdown container if running
-if [ -n "$(docker ps -q -f name=fabric8-test)" ]; then
-    docker rm -f fabric8-test
+if [ -n "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    docker rm -f $CONTAINER_NAME
 fi
 
 exit $RTN_CODE
