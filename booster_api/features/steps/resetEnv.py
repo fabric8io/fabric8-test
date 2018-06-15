601d5fd6d0a719f393b1d8b70aaacffac8777d13
Pavel Macík <pavel.macik@gmail.com>
2018-06-15 13:50:41 +0200

    booster-api: Implement environment reset.

---
+++ booster_api/features/steps/resetEnv.py
@@ -0,0 +1,15 @@
+from behave import *
+from src.resetEnv import ResetEnvironment
+from unittest import *
+
+@when(u'I reset environment')
+def step_impl(context):
+    global resetEnv
+    resetEnv = ResetEnvironment()
+    resetEnv.removeSpaces()
+    resetEnv.cleanTenant()
+
+@then(u'I should see clean environment')
+def step_impl(context):
+    assert len(resetEnv.getSpaces()) == 0
+
