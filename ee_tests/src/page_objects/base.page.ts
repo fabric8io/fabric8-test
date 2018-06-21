90b8dbafe3005ac8760ac41cc2aef0b02319fe99
Pavol Pitonak <ppitonak@redhat.com>
2018-06-21 14:59:29 +0200

    E2E TypeScript upgraded to latest version (#480)

--- ee_tests/src/page_objects/base.page.ts
+++ ee_tests/src/page_objects/base.page.ts
@@ -1,6 +1,5 @@
 import { browser } from 'protractor';
 import * as mixins from '../mixins';
-import * as support from '../support';
 
 export enum PageOpenMode {
   AlreadyOpened,
@@ -12,15 +11,15 @@ export abstract class BasePage {
   // add logging mixin
 
   name: string = '...';
-  log: (action: string, ...msg: string[]) => void;
-  debug: (context: string, ...msg: string[]) => void;
+  log!: (action: string, ...msg: string[]) => void;
+  debug!: (context: string, ...msg: string[]) => void;
 
   // Use undefined to indicate the url has not been set
   // Will use be in openInBrowser to throw error if the caller forgot
   // to set the url. Need to do this because '' is a valid url and
   // refers to the baseUrl
 
-  protected url: string|undefined;
+  protected url: string | undefined;
 
   constructor(url?: string) {
     this.url = url;
@@ -30,7 +29,7 @@ export abstract class BasePage {
   async ready() {
   }
 
-  async open(mode: PageOpenMode =  PageOpenMode.AlreadyOpened): Promise<BasePage> {
+  async open(mode: PageOpenMode = PageOpenMode.AlreadyOpened): Promise<BasePage> {
 
     if (mode === PageOpenMode.RefreshBrowser) {
       await this.openInBrowser();
@@ -50,7 +49,7 @@ export abstract class BasePage {
   }
 
   async openInBrowser() {
-    if (this.url === undefined ) {
+    if (this.url === undefined) {
       throw Error('Trying to open and undefined url');
     }
 
