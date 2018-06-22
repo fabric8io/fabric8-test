49f80657872ce72ef34ca9d8ce8b52a81021a149
Pavol Pitonak <ppitonak@redhat.com>
2018-06-22 10:25:37 +0200

    E2E: login interactions refactored (#506)

--- ee_tests/src/support/index.ts
+++ ee_tests/src/support/index.ts
@@ -1,2 +1,2 @@
 export * from './core';
-export * from './interactions';
+export * from './login_interactions';
--- ee_tests/src/support/login_interactions.ts
+++ ee_tests/src/support/login_interactions.ts
@@ -1,42 +1,21 @@
 import { browser } from 'protractor';
 import { LandingPage, MainDashboardPage } from '../page_objects';
 
-abstract class Interaction {
-
-  async validate() {}
-  async cleanup() {}
-  abstract async perform(): Promise<any>;
-
-  async run() {
-    await this.validate();
-    try {
-      return this.perform();
-    } finally {
-      await this.cleanup();
-    }
-  }
-
-}
-
-export class LoginInteraction extends Interaction {
+export class LoginInteraction {
   page: LandingPage;
   username: string;
   password: string;
 
   constructor(page?: LandingPage, username?: string, password?: string) {
-    super();
     this.username = username || browser.params.login.user;
     this.password = password || browser.params.login.password;
-    // HACK: https://github.com/openshiftio/openshift.io/issues/1402
-    // TODO: remove the hack and use LandingPage instead of OsioLandingPage
-    // when the bug that deletes the tokens is fixed
     this.page = page || new LandingPage();
   }
 
   async validate() {
     expect(this.username === '').toBe(false, 'must provide username');
     expect(this.password === '').toBe(false, 'must provide password');
-    expect(this.page).toBeDefined('page must be intialised');
+    expect(this.page).toBeDefined('page must be initialized');
   }
 
   async perform(): Promise<MainDashboardPage> {
@@ -45,6 +24,11 @@ export class LoginInteraction extends Interaction {
     let dashboard = await loginPage.login(this.username, this.password);
     await dashboard.open();
     return dashboard;
+  }
+
+  async run() {
+    await this.validate();
+    return this.perform();
   }
 }
 
