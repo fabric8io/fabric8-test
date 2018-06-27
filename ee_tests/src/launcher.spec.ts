53df0af5f047c743c273116281cb39a674351edf
Pavel Macík <pavel.macik@gmail.com>
2018-06-27 14:59:00 +0200

    ee_tests: Update launcher.spec test to work at internal level.

--- ee_tests/src/launcher.spec.ts
+++ ee_tests/src/launcher.spec.ts
@@ -1,39 +1,92 @@
-import { browser } from 'protractor';
-
+import { browser, by, element, ExpectedConditions as until } from 'protractor';
 import * as support from './support';
 import { Quickstart } from './support/quickstart';
 
-import { MainDashboardPage } from './page_objects/main_dashboard.page';
-import { AddToSpaceDialog } from './page_objects/space_dashboard/add_to_space_dialog';
+import { SpaceDashboardPage } from './page_objects/space_dashboard.page';
+import { FeatureLevelUtils } from './support/feature_level';
+import { AddToSpaceDialog, PageOpenMode } from './page_objects';
+import { AccountHomeInteractionsFactory } from './interactions/account_home_interactions';
+import { Button } from './ui';
 
-/* Tests to verify the build pipeline */
+describe('Creating a new quickstart in OSIO using ngx launcher', () => {
+  let quickstart: Quickstart;
+  let strategy: string;
+  let spaceName: string;
 
-describe('Creating new quickstart in OSIO', () => {
-  let dashboardPage: MainDashboardPage;
+  beforeAll(async () => {
+    support.info('--- Before all ---');
+    await support.desktopTestSetup();
+    spaceName = support.newSpaceName();
+    strategy = browser.params.release.strategy;
+    quickstart = new Quickstart(browser.params.quickstart.name);
+  });
 
   beforeEach(async () => {
-    await support.desktopTestSetup();
-    let login = new support.LoginInteraction();
-    await login.run();
-    dashboardPage = new MainDashboardPage();
+    support.screenshotManager.nextTest();
   });
 
   afterEach(async () => {
-    support.info('\n ============ End of test reached, logging out ============ \n');
-    await dashboardPage.openInBrowser();
-    await dashboardPage.logout();
+    support.info('--- After each ---');
+    await support.screenshotManager.writeScreenshot('afterEach');
   });
 
-  it('Create a new space, new ' + browser.params.quickstart.name + ' quickstart via ngx Launcher',
-    async () => {
-      let quickstart = new Quickstart(browser.params.quickstart.name);
-      let spaceName = support.newSpaceName();
-      let spaceDashboardPage = await dashboardPage.createNewSpaceByLauncher(spaceName);
-
-      let wizard: AddToSpaceDialog = await spaceDashboardPage.addToSpace();
-
-      await wizard.newQuickstartProjectByLauncher(quickstart.id, spaceName, browser.params.release.strategy);
-      await spaceDashboardPage.ready();
+  afterAll(async () => {
+    support.info('--- After all ---');
+    if (browser.params.reset.environment === 'true') {
+      try {
+        support.info('--- Reset environment ---');
+        let accountHomeInteractions = AccountHomeInteractionsFactory.create();
+        await accountHomeInteractions.resetEnvironment();
+      } catch (e) {
+        await support.screenshotManager.writeScreenshot('resetEnvironment');
+        throw e;
+      }
     }
-  );
+  });
+
+  it('login', async () => {
+    support.info('--- Login ---');
+    let login = new support.LoginInteraction();
+    await login.run();
+  });
+
+  it('feature_level', async () => {
+    support.info('--- Check if feature level is set correctly ---');
+    // FIXME: Uncomment once the Dependency editor is promoted to beta.
+    // let featureLevel = await FeatureLevelUtils.getRealFeatureLevel();
+    // expect(featureLevel).toBe(FeatureLevelUtils.getConfiguredFeatureLevel(), 'feature level');
+  });
+
+  it('create_space_new_codebase', async () => {
+
+    // WORKAROUND: For the internal features level.
+    support.info('--- Create space with new codebase ' + spaceName + ' ---');
+    let accountHomeInteractions = AccountHomeInteractionsFactory.create();
+    await accountHomeInteractions.createSpace(spaceName);
+    let spaceDashboardPage = new SpaceDashboardPage(spaceName);
+    await spaceDashboardPage.open(PageOpenMode.RefreshBrowser);
+    await spaceDashboardPage.ready();
+    if (FeatureLevelUtils.isInternal()) {
+      support.info('Using [Create an application] button');
+      let createAnApplicationButton = new Button(
+        element(by.id('user-level-analyze-overview-dashboard-create-space-button')),
+        'Create an Application'
+      );
+      await createAnApplicationButton.clickWhenReady();
+    } else {
+      support.info('Using [Add to Space] button');
+      await spaceDashboardPage.addToSpaceButton.clickWhenReady();
+    }
+    support.info('Creating application from new codebase');
+    let wizard = new AddToSpaceDialog(element(by.xpath('//f8-add-app-overlay')));
+    await browser.wait(until.presenceOf(element(by.cssContainingText('div', 'Create an Application'))));
+    await wizard.newQuickstartProjectByLauncher(quickstart.id, spaceName, strategy);
+    support.info('Application created');
+    // END-OF-WORKAROUND
+
+    // TODO: replace the above WORKAROUND by the following once the Dependency editor is promoted to beta.
+    // support.info('--- Create space with new codebase ' + spaceName + ' ---');
+    // let accountHomeInteractions = AccountHomeInteractionsFactory.create();
+    // await accountHomeInteractions.createSpaceWithNewCodebase(spaceName, quickstart.name, strategy);
+  });
 });
