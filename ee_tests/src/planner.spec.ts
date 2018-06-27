d41ba94fff51260c7a7798545696fd40f0285ae2
Raunak Bajpai <bajpairaunak@gmail.com>
2018-06-27 18:58:51 +0530

    updated tests

--- ee_tests/src/planner.spec.ts
+++ ee_tests/src/planner.spec.ts
@@ -3,7 +3,7 @@ import * as support from './support';
 import { MainDashboardPage, SpaceDashboardPage, SpaceSettings } from './page_objects';
 import  { PlannerPage } from 'planner-functional-test';
 
-describe('Adding areas and Collaborators to a space', () => {
+describe('Planner EE tests adding Area and Collaborator', () => {
   let dashboardPage: MainDashboardPage,
     SettingsPage = new SpaceSettings(),
     planner = new PlannerPage(browser.baseUrl),
@@ -21,7 +21,8 @@ describe('Adding areas and Collaborators to a space', () => {
     await dashboardPage.waitUntilUrlContains(spaceName);
   });
 
-  it('should Navigate to settings and add New Area', async () => {
+  it('should Navigate to settings, add New Area and assign it to a workitem ', async () => {
+    // Navigate to settings, add Area
     await SettingsPage.clickSettings();
     expect(await browser.getCurrentUrl()).toContain('settings');
     await SettingsPage.list.untilCount(1);
@@ -29,6 +30,8 @@ describe('Adding areas and Collaborators to a space', () => {
     await SettingsPage.addAreas('New Area');
     await SettingsPage.clickShowAreas();
     expect(await SettingsPage.list.getText()).toContain('New Area');
+
+    // Click Plan tab, Create a new work Item and Add new Area
     let workitemname = {"title": "area test"};
     await new SpaceDashboardPage(spaceName).planTab.clickWhenReady();
     await planner.waitUntilUrlContains('typegroup');
@@ -41,7 +44,8 @@ describe('Adding areas and Collaborators to a space', () => {
     await planner.quickPreview.close();
   });
 
-  it('should Navigate to collaborators and add a new collaborator', async () => {
+  it('should Navigate to collaborators, add a new collaborator and assign it to a workitem ', async () => {
+    // Navigate to settings, add collaborator
     let user = browser.params.login.user;
     let workitemname = {"title": "collaborator test"};
 
@@ -52,6 +56,8 @@ describe('Adding areas and Collaborators to a space', () => {
     expect(await SettingsPage.getCollaboratorList()).toContain(user);
     await SettingsPage.addCollaborators('Osio10');
     expect(await SettingsPage.getCollaboratorList()).toContain('Osio10');
+
+    // Click Plan tab, Create a new work Item and Assign it to Collaborator
     await new SpaceDashboardPage(spaceName).planTab.clickWhenReady();
     await planner.waitUntilUrlContains('typegroup');
     await planner.ready();
