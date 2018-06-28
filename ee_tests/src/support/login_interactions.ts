dc62c0d786490f3699fb60704ad3e17eae6d0dd9
Pavol Pitonak <ppitonak@redhat.com>
2018-06-28 09:44:36 +0200

    E2E: import in LoginInteraction updated (related to #805)

--- ee_tests/src/support/login_interactions.ts
+++ ee_tests/src/support/login_interactions.ts
@@ -1,6 +1,6 @@
 import { browser } from 'protractor';
 import { LandingPage } from '../page_objects/landing.page';
-import { PageOpenMode } from '../page_objects/base.page';
+import { PageOpenMode } from '../page_objects';
 import { AccountHomeInteractionsFactory } from '../interactions/account_home_interactions';
 
 export class LoginInteraction {
