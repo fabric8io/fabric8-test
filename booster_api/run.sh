557131c838f367c98fce022bd883e757d9b3f1a9
Pavel Macík <pavel.macik@gmail.com>
2018-07-02 18:24:30 +0200

    booster-api: Add content to the README.md

--- booster_api/run.sh
+++ booster_api/run.sh
@@ -2,6 +2,7 @@
 source config/config.sh
 
 echo "Starting behave testsuite"
-time behave -f allure_behave.formatter:AllureFormatter -o $REPORT_DIR --no-capture --no-capture-stderr @features_list.txt
-#behave --junit --junit-directory $REPORT_DIR --no-capture-stderr --no-logcapture @features_list.txt
+
+behave -f allure_behave.formatter:AllureFormatter -o $REPORT_DIR --no-capture --no-capture-stderr @features_list.txt
+
 echo "behave testsuite COMPLETE"
