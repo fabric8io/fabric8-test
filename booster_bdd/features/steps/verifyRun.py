9e1253e2d3a1be093b709ef42d001b090c9fdba3
ldimaggi <ldimaggi@redhat.com>
2018-07-04 14:20:38 -0400

    (fix)test: add test to verify deploy to run

--- booster_bdd/features/steps/verifyRun.py
+++ booster_bdd/features/steps/verifyRun.py
@@ -1,17 +1,21 @@
 from behave import *
-
+from src.run import *
+from unittest import *
 
 @given(u'I have verified a booster\'s pipeline has had its deployment to stage verified')
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Given I have verified a booster\'s pipeline has had its deployment to stage verified')
-
+    print ('Attempting to use query for running Pipeline...')
+    global run
+    run = Run()
 
 @when(u'I query a pipeline\'s run endpoint')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I query a pipeline\'s run endpoint')
-
+    global result
+    result = run.runTest('testing 1234567890')
+    print ('Result = {}'.format(result))
 
 @then(u'I should see the deployed app running on run')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should see the deployed app running on run')
+    global expected_result
+    expected_result = 'Success'
+    assert (expected_result == result)
