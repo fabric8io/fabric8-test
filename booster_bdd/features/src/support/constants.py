518eba8b191233138e26d650e4740db103695230
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-09 11:38:44 +0200

    Fixed Pylint warnings in constants.py module

--- booster_bdd/features/src/support/constants.py
+++ booster_bdd/features/src/support/constants.py
@@ -5,33 +5,34 @@ import json
 
 
 class launch_details:
-    launch_details_dict = {}  ###A dict that would be dumped on teh disk for UI tests to use
-    test_on_prod = True  #### Set this flag if running tests on prod
+    launch_details_dict = {}  # A dict that would be dumped on the disk for UI tests to use
+    test_on_prod = True  # Set this flag if running tests on prod
 
-    ###USERIDs
-    userid_prod_primary_default = 'rgarg-osiotest1'  ####  DEFAULT PROD OSIO USER ID
+    # USERIDs
+    userid_prod_primary_default = 'rgarg-osiotest1'  # DEFAULT PROD OSIO USER ID
     userid_primary = userid_prod_primary_default
 
-    ###TOKENs
+    # TOKENs
     offref_token_userid_primary = None
     token_userid_primary = None
 
-    ###BASE URLs
+    # BASE URLs
     base_wit = "base_wit"
     base_forge = "base_forge"
     base_url = {}
 
     def get_keycloak_token(self):
-        ###Used only for local token generation. Not used for prod or prod-preview
+        # Used only for local token generation. Not used for prod or prod-preview
         url = self.create_url('api/login/generate')
         r = requests.get(url)
         return self.extract_value("[0].token.access_token", r)
 
     def get_access_token_from_refresh(self):
-        ###Access the WIT endpoint to obtain access token from the refresh token
+        """Access the WIT endpoint to obtain access token from the refresh token."""
         url = self.create_url("api/login/refresh")
-        payload = {"refresh_token":self.offref_token_userid_primary}
-        r = requests.post(url, headers={'Content-Type': r'application/json'}, data = json.dumps(payload))
+        payload = {"refresh_token": self.offref_token_userid_primary}
+        r = requests.post(url, headers={'Content-Type': r'application/json'},
+                          data=json.dumps(payload))
         return self.extract_value("token.access_token", r)
 
     def create_url(self, api, base_target=base_wit):
@@ -39,16 +40,19 @@ class launch_details:
 
     def extract_value(self, extract_path=None, json_response=None):
         if None in [json_response, extract_path]:
-            print "Either Json response or the extractor path are None"
+            print("Either JSON response or the extractor path are None")
             return None
         else:
             try:
                 return jmespath.search(extract_path, json_response.json())
-            except:
-                print "Exception extracting value from the response body"
+            except Exception:
+                print("Exception extracting value from the response body")
                 return None
+
+
 launch_detail = launch_details()
- 
+
+
 class requests_constants:
     content_type_key_default = 'Content-Type'
     content_type_default = r'application/vnd.api+json'
@@ -57,9 +61,11 @@ class requests_constants:
     authorization_carrier_default = 'Bearer '
     headers_default = None
 
+
 request_detail = requests_constants()
 
-###To-DO: The following UUIDs will be later fetched using an API
+
+# To-DO: The following UUIDs will be later fetched using an API
 class workitem_constants:
     witypescenario = "71171e90-6d35-498f-a6a7-2083b5267c18"
     witypefundamental = "ee7ca005-f81d-4eea-9b9b-1965df0988d0"
@@ -106,7 +112,7 @@ class dynamic_data:
         self.spacename = None
         self.spacelink = None
 
-        ###Planner
+        # Planner
         self.parent_area_id = None
         self.parent_area_name = None
         self.area_names_to_ids = {}
