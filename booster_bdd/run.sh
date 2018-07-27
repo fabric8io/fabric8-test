ae0aee66d815d0dd8df377455b2872c3d9b5555c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-27 13:59:28 +0200

    Fix common BASH-related issues in booster_bdd

--- booster_bdd/run.sh
+++ booster_bdd/run.sh
@@ -4,7 +4,7 @@ CONFIG=${1:-config}
 CONFIG_FILE="config/$CONFIG.sh"
 
 function prepare_venv() {
-	python3 -m venv venv && source venv/bin/activate && python3 $(which pip3) install -r requirements.txt
+	python3 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
@@ -12,7 +12,7 @@ function prepare_venv() {
 echo "Reading configuration from: $CONFIG_FILE"
 source "$CONFIG_FILE"
 
-if [ -z $SCENARIO ]; then
+if [ -z "$SCENARIO" ]; then
 	echo "Running all tests ..."
 	export feature_list=test-scenarios/feature_list
 	cat test-scenarios/*.test >$feature_list
@@ -21,10 +21,10 @@ else
 	export feature_list=test-scenarios/$SCENARIO.test
 fi
 
-PYTHONDONTWRITEBYTECODE=1 python3 $(which behave) -v -f allure_behave.formatter:AllureFormatter -o "$REPORT_DIR" --tags="osio.regular,${BEHAVE_DANGER_TAG:-~@osio.danger-zone}" --no-capture --no-capture-stderr @$feature_list
+PYTHONDONTWRITEBYTECODE=1 python3 "$(which behave)" -v -f allure_behave.formatter:AllureFormatter -o "$REPORT_DIR" --tags="osio.regular,${BEHAVE_DANGER_TAG:-~@osio.danger-zone}" --no-capture --no-capture-stderr @$feature_list
 
 echo "All tests are done!"
 
-if [ -z $SCENARIO ]; then
-	rm -rvf $feature_list
+if [ -z "$SCENARIO" ]; then
+	rm -rvf "$feature_list"
 fi
