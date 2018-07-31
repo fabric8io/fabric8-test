3d47dda7fff328698503ddbc8196848066b3fef9
Lucia Jelinkova <ljelinko@redhat.com>
2018-07-31 12:51:35 +0200

    E2E: Change recent spaces widget on prod

--- ee_tests/src/interactions/account_home_interactions.ts
+++ ee_tests/src/interactions/account_home_interactions.ts
@@ -15,13 +15,6 @@ export abstract class AccountHomeInteractionsFactory {
             return new NewAccountHomeInteractions(new AccountHomePage());
         }
 
-        let url: string = browser.params.target.url;
-        let isProdPreview = url.includes('prod-preview');
-
-        if (isProdPreview === true) {
-            return new ProdPreviewOldAccountHomeInteractions(new MainDashboardPage());
-        }
-
         return new OldAccountHomeInteractions(new MainDashboardPage());
     }
 }
@@ -86,17 +79,6 @@ class OldAccountHomeInteractions extends AbstractSpaceDashboardInteractions {
 
     public async openSpaceDashboard(name: string): Promise<void> {
         await this.dashboardPage.openSpace(name);
-    }
-}
-
-class ProdPreviewOldAccountHomeInteractions extends OldAccountHomeInteractions {
-
-    constructor(page: MainDashboardPage) {
-        super(page);
-    }
-
-    public async openSpaceDashboard(name: string): Promise<void> {
-        await this.dashboardPage.openSpaceProdPreview(name);
     }
 }
 
