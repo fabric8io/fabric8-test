b22e18483f79c5b6865413ac4901fbd3c5be6c2d
Pavol Pitonak <ppitonak@redhat.com>
2018-08-09 09:02:26 +0200

    e2e: main dashboard updated

--- ee_tests/src/page_objects/main_dashboard.page.ts
+++ ee_tests/src/page_objects/main_dashboard.page.ts
@@ -18,8 +18,8 @@ export class MainDashboardPage extends AppPage {
       support.DEFAULT_WAIT, 'Recent Spaces is present');
     await browser.wait(until.presenceOf(element(by.cssContainingText('div', 'My Work Items'))),
       support.DEFAULT_WAIT, 'My Work Items title is present');
-    await browser.wait(until.presenceOf(element(by.cssContainingText('div', 'Recent Active Pipelines'))),
-      support.DEFAULT_WAIT, 'Recent Active Pipelines title is present');
+    await browser.wait(until.presenceOf(element(by.id('recent-pipelines-card'))),
+      support.DEFAULT_WAIT, 'Recent Pipelines title is present');
   }
 
   async openUsingMenu() {
@@ -27,16 +27,6 @@ export class MainDashboardPage extends AppPage {
   }
 
   async openSpace(spaceName: string): Promise<SpaceDashboardPage> {
-    await browser.wait(until.presenceOf(element(by.className('home-space-list-result'))),
-      support.DEFAULT_WAIT, 'Tag with home-space-list-result class name is present');
-    await element(by.className('home-space-list-result')).element(by.cssContainingText('a', spaceName)).click();
-
-    let spaceDashboard = new SpaceDashboardPage(spaceName);
-    await spaceDashboard.open();
-    return Promise.resolve(spaceDashboard);
-  }
-
-  async openSpaceProdPreview(spaceName: string): Promise<SpaceDashboardPage> {
     await browser.wait(until.presenceOf(element(by.className('f8-home-space-list-result'))),
       support.DEFAULT_WAIT, 'Tag with f8-home-space-list-result class name is present');
     await element(by.className('f8-home-space-list-result')).element(by.cssContainingText('a', spaceName)).click();
@@ -44,5 +34,9 @@ export class MainDashboardPage extends AppPage {
     let spaceDashboard = new SpaceDashboardPage(spaceName);
     await spaceDashboard.open();
     return Promise.resolve(spaceDashboard);
+  }
+
+  async openSpaceProdPreview(spaceName: string): Promise<SpaceDashboardPage> {
+    return this.openSpace(spaceName);
   }
 }
