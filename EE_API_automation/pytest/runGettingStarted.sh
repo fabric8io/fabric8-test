8146db99f47d367fee243e214848af55f9ad131b
Raunak Bajpai <bajpairaunak@gmail.com>
2018-08-20 12:36:35 +0530

    fix(test) : enable verbose logging

--- EE_API_automation/pytest/runGettingStarted.sh
+++ EE_API_automation/pytest/runGettingStarted.sh
@@ -23,7 +23,7 @@ else
         echo "==> Launching the GettingStarted API tests to generate TestDB..."
         echo ""
         cd src/
-        pytest test_getting_started.py -s --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 2>&1 | tee $CLILOG
+        pytest test_getting_started.py -s -v --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 2>&1 | tee $CLILOG
         echo ""
 
     elif [ "$4" = "True" ] || [ "$4" = "true" ]
@@ -31,7 +31,7 @@ else
         echo "==> Launching the GettingStarted API tests to check GettingStarted sanity..."
         echo ""
         cd src/
-        pytest test_getting_started.py -s --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 --cleanup=$4 2>&1 | tee $CLILOG
+        pytest test_getting_started.py -s -v --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 --cleanup=$4 2>&1 | tee $CLILOG
         echo $?
     fi
 fi
