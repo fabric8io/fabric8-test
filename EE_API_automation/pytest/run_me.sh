3a3df0e2c09a09c84d7a26316411bce74b1e86e3
Raunak Bajpai <bajpairaunak@gmail.com>
2018-08-20 12:36:35 +0530

    fix(test) : enable verbose logging

--- EE_API_automation/pytest/run_me.sh
+++ EE_API_automation/pytest/run_me.sh
@@ -24,7 +24,7 @@ else
         echo "==> Launching the Planner API tests to generate TestDB..."
         echo ""
         cd src/
-        pytest test_gen_testdb.py -s --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 2>&1 | tee $CLILOG
+        pytest test_gen_testdb.py -s -v --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 2>&1 | tee $CLILOG
         echo ""
     elif [ "$4" = "True" ] || [ "$4" = "true" ]
     then
@@ -32,7 +32,7 @@ else
         echo "==> Launching the Planner API tests to check Planner sanity..."
         echo ""
         cd src/
-        pytest test_planner.py -s --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 --cleanup=$4 2>&1 | tee $CLILOG
+        pytest test_planner.py -s -v --junitxml=$JENKINSLOG --sut=$1 --userid=$2 --offline_token=$3 --cleanup=$4 2>&1 | tee $CLILOG
         echo $?
     fi
 fi
