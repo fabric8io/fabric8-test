a38af2b6c99a99bddb978e99c5c93cb81686a93d
Pavel Macík <pavel.macik@gmail.com>
2018-08-22 13:04:21 +0200

    booster-bdd: Cleanup CICO job.

--- booster_bdd/local_run.sh
+++ booster_bdd/local_run.sh
@@ -12,6 +12,29 @@ function prepare_venv() {
 echo "Reading configuration from: $CONFIG_FILE"
 source "$CONFIG_FILE"
 
+echo "SCENARIO=$SCENARIO"
+echo "SERVER_ADDRESS=$SERVER_ADDRESS"
+echo "FORGE_API=$FORGE_API"
+echo "WIT_API=$WIT_API"
+echo "AUTH_API=$AUTH_API"
+echo "OSIO_USERNAME=$OSIO_USERNAME"
+echo "OSIO_PASSWORD=$OSIO_PASSWORD"
+echo "OSIO_DANGER_ZONE=$OSIO_DANGER_ZONE"
+echo "PIPELINE=$PIPELINE"
+echo "BOOSTER_MISSION=$BOOSTER_MISSION"
+echo "BOOSTER_RUNTIME=$BOOSTER_RUNTIME"
+echo "BLANK_BOOSTER=$BLANK_BOOSTER"
+echo "GIT_REPO=$GIT_REPO"
+echo "PROJECT_NAME=$PROJECT_NAME"
+echo "AUTH_CLIENT_ID=$AUTH_CLIENT_ID"
+echo "REPORT_DIR=$REPORT_DIR"
+echo "UI_HEADLESS=$UI_HEADLESS"
+
+echo "BEHAVE_DANGER_TAG=$BEHAVE_DANGER_TAG"
+echo "OSO_USERNAME=$OSO_USERNAME"
+#echo "OSO_TOKEN=$OSO_TOKEN"
+echo "GITHUB_USERNAME=$GITHUB_USERNAME"
+
 if [ -z "$SCENARIO" ]; then
 	echo "Running all tests ..."
 	export feature_list=test-scenarios/feature_list
@@ -21,7 +44,7 @@ else
 	export feature_list=test-scenarios/$SCENARIO.test
 fi
 
-PYTHONDONTWRITEBYTECODE=1 python3 "$(which behave)" -v -f allure_behave.formatter:AllureFormatter -o "$REPORT_DIR" --tags="osio.regular,${BEHAVE_DANGER_TAG:-~@osio.danger-zone}" --no-capture --no-capture-stderr @$feature_list
+PYTHONDONTWRITEBYTECODE=1 python3 "$(which behave)" -v -f allure_behave.formatter:AllureFormatter -o "$REPORT_DIR" --tags="@osio.regular,${BEHAVE_DANGER_TAG:-~@osio.danger-zone}" --no-capture --no-capture-stderr @$feature_list
 
 echo "All tests are done!"
 
