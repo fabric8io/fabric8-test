65c886509158d6cdc10152e48e99dc687e8dda5d
Pavel Macík <pavel.macik@gmail.com>
2018-08-28 15:47:30 +0200

    booster-bdd: Implement fail-fast checks for Run and Stage.

--- booster_bdd/features/steps/verifyStage.py
+++ booster_bdd/features/steps/verifyStage.py
@@ -1,13 +1,12 @@
 from behave import given, when, then
 from features.src.stage import Stage
-from features.src.pipeline import pipelineVerified
+from features.src import pipeline
 from pyshould import should
 
 
 @given(u'I have verified a booster\'s pipeline has completed')
 def given_pipeline_completed(_context):
-    global pipelineVerified
-    pipelineVerified | should.be_true.desc("Pipeline verified")
+    pipeline.pipelineVerified | should.be_true.desc("Pipeline verified")
     print('Attempting to use query for Pipeline deployed to Stage...')
     global stage
     stage = Stage()
