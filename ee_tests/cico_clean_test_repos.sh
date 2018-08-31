1c699fe9eb290e34de5e97d53187a6b4aac94e10
Pavol Pitonak <ppitonak@redhat.com>
2018-08-31 09:45:26 +0200

    e2e: cleanup script improvements

--- ee_tests/cico_clean_test_repos.sh
+++ ee_tests/cico_clean_test_repos.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-yum install -y epel-release
-yum install jq
+#stop on error
+set -e
 
-yes | ./local_clean_test_repos.sh
+yum install -y epel-release
+yum install -y jq
+
+# Export needed vars
+set +x
+for var in GITHUB_TOKEN; do
+  export "$(grep ${var} ../jenkins-env | xargs)"
+done
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "required variable GITHUB_TOKEN is not defined"
+  exit 1
+fi
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+yes | $DIR/local_clean_test_repos.sh
