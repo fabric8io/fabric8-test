d3540c2357ea23ae567d68fa88c6f1afd5f04b25
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-01 11:55:20 +0200

    Very minor fix: the 'second' parameter is used by test step implementation

--- booster_bdd/features/steps/launchBooster.py
+++ booster_bdd/features/steps/launchBooster.py
@@ -32,7 +32,7 @@ def when_input_booster(_context):
     print('Result = {}'.format(result))
 
 
-@then(u'I should see the booster created within {_seconds} seconds')
-def then_booster_created(_context, _seconds):
-    lb.checkCodebases(_seconds)
+@then(u'I should see the booster created within {seconds} seconds')
+def then_booster_created(_context, seconds):
+    lb.checkCodebases(seconds)
     launchBooster.boosterLaunched | should.be_true.desc("Booster created.")
