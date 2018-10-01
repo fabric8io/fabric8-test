d7208c49fb1488f94a6a6d040ca2e850c27d1beb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-01 11:13:54 +0200

    Added docstring comments

--- booster_bdd/features/environment.py
+++ booster_bdd/features/environment.py
@@ -1,8 +1,10 @@
+"""Module with code to be run before and after certain events during the testing."""
 import os
 from src.support import helpers
 
 
 def before_all(_context):
+    """Perform the setup before the first event."""
     if not helpers.is_user_logged_in():
         username = os.getenv("OSIO_USERNAME")
         password = os.getenv("OSIO_PASSWORD")
