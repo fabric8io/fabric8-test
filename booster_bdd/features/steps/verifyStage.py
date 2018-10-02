aa5022a4246b0f21e08aac8c1fe84b6ae45b6254
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-02 12:45:22 +0200

    Added docstrings + don't use global vars in verifyStage

--- booster_bdd/features/steps/verifyStage.py
+++ booster_bdd/features/steps/verifyStage.py
@@ -1,3 +1,5 @@
+"""Steps implementation for following scenarios: verifyStage.feature."""
+
 from behave import given, when, then
 from features.src.stage import Stage
 from features.src import pipeline
@@ -5,21 +7,23 @@ from pyshould import should
 
 
 @given(u'I have verified a booster\'s pipeline has completed')
-def given_pipeline_completed(_context):
+def given_pipeline_completed(context):
+    """Precondition that the pipeline has been verified."""
     pipeline.pipelineVerified | should.be_true.desc("Pipeline verified")
     print('Attempting to use query for Pipeline deployed to Stage...')
-    global stage
-    stage = Stage()
+    context.stage = Stage()
 
 
 @when(u'I query a pipeline\'s stage endpoint')
-def when_query_pipeline(_context):
-    global result
-    result = stage.runTest('testing stage endpoint')
+def when_query_pipeline(context):
+    """Query a pipeline's stage endpoint."""
+    result = context.stage.runTest('testing stage endpoint')
     print('Result = {}'.format(result))
+    context.result = result
 
 
 @then(u'I should see the deployed app running on stage')
-def then_app_running_stage(_context):
-    global result
+def then_app_running_stage(context):
+    """Check that the app is deployed and running on stage."""
+    result = context.result
     result | should.equal('Success').desc("Application is not reachable in the Stage stage.")
