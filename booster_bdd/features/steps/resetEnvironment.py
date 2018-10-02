1d96bb9225b6e24764656e6e0f12dae06611c06b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-02 12:26:27 +0200

    Added docstrings + don't use global vars in resetEnvironment

--- booster_bdd/features/steps/resetEnvironment.py
+++ booster_bdd/features/steps/resetEnvironment.py
@@ -1,19 +1,22 @@
+"""Steps implementation for following scenarios: reset user's environment in OpenShift.io."""
+
 from behave import when, then
 from features.src.resetEnv import ResetEnvironment
 from pyshould import should
 
 
 @when(u'I reset environment')
-def when_reset_environment(_context):
-    global resetEnv
+def when_reset_environment(context):
+    """Reset the environment."""
     resetEnv = ResetEnvironment()
     resetEnv.removeSpaces()
     resetEnv.cleanTenant()
+    context.resetEnv = resetEnv
 
 
 @then(u'I should see clean environment')
-def then_clean_environment(_context):
-    global resetEnv
-    len(resetEnv.getSpaces()) | should.equal(0).described_as(
+def then_clean_environment(context):
+    """Check that environment has been reset."""
+    len(context.resetEnv.getSpaces()) | should.equal(0).described_as(
         "Number of spaces after environment reset."
     )
