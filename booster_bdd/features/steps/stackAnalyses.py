1aeefa9c997509757cb9b78b58d1c52ff1687c88
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-03 13:15:45 +0200

    Removed unused imports

--- booster_bdd/features/steps/stackAnalyses.py
+++ booster_bdd/features/steps/stackAnalyses.py
@@ -1,7 +1,7 @@
 from behave import when, then
 from features.src.support import helpers
 from features.src.stackAnalyses import StackAnalyses
-from pyshould import should_not, should
+from pyshould import should_not
 
 @when(u'I send Maven package manifest pom-effective.xml to stack analysis')
 def when_send_manifest(context):
