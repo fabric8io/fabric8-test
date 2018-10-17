c58137317c8987a78b2ed2cde602d79f2c36463d
ldimaggi <ldimaggi@redhat.com>
2018-10-17 14:16:50 -0400

    add effective pom

--- booster_bdd/test.sh
+++ booster_bdd/test.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-set -x 
+# Shell script to create/update to master effective pom
+
+set +x
 
 echo "Create temp dir"
 mkdir tempDir
@@ -9,25 +11,34 @@ echo "cd to temp dir"
 cd tempDir
 
 echo "clone the github repo"
-git clone git@github.com:ldimaggi/$1.git
+git clone $1
 
 echo "cd to the cloned github repo"
 cd $2
 
-echo "create the effective pom"
-mvn help:effective-pom -Doutput=effective-pom.xml
+echo "show the dir name"
+pwd
 
-echo "copy the effective pom over pom.xml"
-cp effective-pom.xml pom.xml 
+echo "show the dir contents"
+ls -l
+
+echo "rename original pom.xml"
+mv pom.xml epom.xml
+
+echo "create the effective pom"
+mvn help:effective-pom -f epom.xml -Doutput=pom.xml
+
+echo "set the git username and password"
+git config --global user.name $3
+git config --global user.password $4
 
 echo "commit the effective-pom to the master branch"
-git commit -a -m "add effective pom"
+git commit -a -m "add effective pom" 
 
 echo "push the updates to master"
-git push -f
+git push -f      
 
 echo "cd back to the starting dir"
-cd ..
-
+cd ../..
 
 
