81921101a9b37a9f9f9eade9f64dd9b172b3c450
osioperftest <pavel.macik@gmail.com>
2018-11-14 15:03:30 +0100

    contract-tests: Use docker-compose to start local auth service.

--- contract_tests/go/auth-api/support/start-local-auth.sh
+++ contract_tests/go/auth-api/support/start-local-auth.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
 set -x
-
-export GOPATH="$(pwd)/local-gopath"
-mkdir -p "$GOPATH"
-
-go get -u github.com/fabric8-services/fabric8-auth
-
-cd $GOPATH/src/github.com/fabric8-services/fabric8-auth
-
-make dev
+docker-compose -f docker-compose.auth.yml up
