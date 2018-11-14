f4eef43a6ba73d04e94a34b659d710f870a7721b
Len DiMaggio <ldimaggi@redhat.com>
2018-11-14 11:15:23 -0500

    Revert "contract-tests: Use docker-compose to start local auth service."
    This reverts commit 81921101a9b37a9f9f9eade9f64dd9b172b3c450.

--- contract_tests/go/auth-api/support/start-local-auth.sh
+++ contract_tests/go/auth-api/support/start-local-auth.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
 set -x
-docker-compose -f docker-compose.auth.yml up
+
+export GOPATH="$(pwd)/local-gopath"
+mkdir -p "$GOPATH"
+
+go get -u github.com/fabric8-services/fabric8-auth
+
+cd $GOPATH/src/github.com/fabric8-services/fabric8-auth
+
+make dev
