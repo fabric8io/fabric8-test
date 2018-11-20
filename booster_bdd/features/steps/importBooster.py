01a70a4d058d5c10d31c96e121672703362c831e
osioperftest <pavel.macik@gmail.com>
2018-11-20 13:03:55 +0100

    booster-bdd: Update importBooster feature impl.

--- booster_bdd/features/steps/importBooster.py
+++ booster_bdd/features/steps/importBooster.py
@@ -9,22 +9,21 @@ from pyshould import should, should_not
 
 @given(u'I have a space created')
 def given_space_created(_context):
-    global spaceID
-    spaceID = helpers.getSpaceID()
-    spaceID | should_not.be_none().desc("Space ID exists.")
+    _context.spaceID = helpers.getSpaceID()
+    _context.spaceID | should_not.be_none().desc("Space ID exists.")
 
-    global ib
-    ib = ImportBooster()
+    _context.ib = ImportBooster()
 
 
 @when(u'I input a name of the GitHub repository with a booster')
 def when_input_github_repo(_context):
     helpers.setGithubRepo(os.getenv('GIT_REPO'))
     importBooster.boosterImported | should.be_false.desc("Booster is not imported, yet.")
-    result = ib.importGithubRepo(helpers.getGithubRepo())
+    result = _context.ib.importGithubRepo(helpers.getGithubRepo())
     print('Result = {}'.format(result))
 
 
-@then(u'I should see the booster imported')
-def then_booster_imported(_context):
+@then(u'I should see the booster imported within {seconds} seconds')
+def then_booster_imported(_context, seconds):
+    _context.ib.checkCodebases(seconds)
     importBooster.boosterImported | should.be_true.desc("A GitHub repository is imported.")
