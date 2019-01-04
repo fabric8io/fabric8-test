f0b8b091e5e356f0b3950b107051c4dc419b4d54
ldimaggi <ldimaggi@redhat.com>
2019-01-04 16:32:32 -0500

    fix(test): poc test for preboosters

---
+++ booster_upstream/cico_run_prebooster_tests.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# Do not reveal secrets
+set +x
+
+# Do not exit on failure so that artifacts can be archived
+set +e
+
+# Source environment variables of the jenkins slave
+# that might interest this worker.
+if [ -e "../jenkins-env" ]; then
+  grep -E "(JENKINS_URL|\
+    |GIT_BRANCH|\
+    |GIT_COMMIT|\
+    |JOB_NAME|\
+    |BUILD_NUMBER|\
+    |ghprbSourceBranch|\
+    |ghprbActualCommit|\
+    |BUILD_URL|\
+    |ghprbPullId|\
+    |PROJECT_NAME|\
+    |BOOSTER_NAME|\
+    |CLUSTER|\
+    |BOOSTER_REPO|\
+    |OS_USERNAME|\
+    |OS_PASSWORD|\
+    |REPORT_DIR|\
+    |UI_HEADLESS|\
+    |ZABBIX_ENABLED|\
+    |ZABBIX_SERVER|\
+    |ZABBIX_HOST|\
+    |ZABBIX_METRIC_PREFIX)=" ../jenkins-env \
+    | sed 's/^/export /g' \
+    > /tmp/jenkins-env
+  source /tmp/jenkins-env
+fi
+
+# Assign default values if not defined in Jenkins job
+
+## An output directory where the reports will be stored
+export REPORT_DIR=${REPORT_DIR:-target}
+
+## 'true' if the UI parts of the test suite are to be run in headless mode (default value is 'true')
+export UI_HEADLESS=${UI_HEADLESS:-true}
+
+export ZABBIX_ENABLED="${ZABBIX_ENABLED:-false}"
+
+export ZABBIX_SERVER="${ZABBIX_SERVER:-zabbix.devshift.net}"
+
+export ZABBIX_HOST="${ZABBIX_HOST:-qa_openshift.io}"
+
+export ZABBIX_METRIC_PREFIX="${ZABBIX_METRIC_PREFIX:-booster-bdd.$SCENARIO}"
+
+export ARTIFACTS_DIR="prebooster/${JOB_NAME}/${BUILD_NUMBER}"
+mkdir -p "$ARTIFACTS_DIR"
+LATEST_LINK_PATH="prebooster/${JOB_NAME}/latest"
+ln -sfn "$BUILD_NUMBER" "$LATEST_LINK_PATH"
+
+export TEST_LOG="$ARTIFACTS_DIR/test.log"
+
+echo "Running the test while redirecting the output to $TEST_LOG ..."
+
+{ ## output only to $TEST_LOG
+  # If report dir did exist, remove artifacts from previous run
+  rm -rf "$REPORT_DIR"
+  mkdir -p "$REPORT_DIR"
+
+  # We need to disable selinux for now
+  /usr/sbin/setenforce 0
+  yum -y install docker 
+  service docker start
+
+  export CONTAINER_NAME="fabric8-booster-test"
+
+  # Shutdown container if running
+  if [ -n "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+      docker rm -f "$CONTAINER_NAME"
+  fi
+
+  # Build builder image
+  cp /tmp/jenkins-env .
+  docker build -t $CONTAINER_NAME:latest -f Dockerfile.builder .
+
+  # Run and setup Docker image
+  docker run -it --shm-size=256m --detach=true --name="$CONTAINER_NAME" --cap-add=SYS_ADMIN \
+            -e PROJECT_NAME \
+            -e BOOSTER_NAME \
+            -e CLUSTER \
+            -e BOOSTER_REPO \
+            -e OS_USERNAME \
+            -e OS_PASSWORD \
+            -e REPORT_DIR \
+            -e UI_HEADLESS \
+            -e ZABBIX_SERVER \
+            -e ZABBIX_HOST \
+            -e ZABBIX_METRIC_PREFIX \
+            -t -v /etc/localtime:/etc/localtime:ro "$CONTAINER_NAME:latest" /bin/bash
+
+  # Exec booster tests
+  docker exec "$CONTAINER_NAME" ./prebooster_cico.sh
+
+  if [ "${ZABBIX_ENABLED,,}" = true ] ; then
+      docker exec "$CONTAINER_NAME" zabbix_sender -vv -T -i "./$REPORT_DIR/zabbix-report.txt" -z "$ZABBIX_SERVER"
+  fi
+
+  # Test results to archive
+  docker cp "$CONTAINER_NAME:/opt/fabric8-test/$REPORT_DIR/." "$ARTIFACTS_DIR"
+
+  # Shutdown container if running
+  if [ -n "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+      docker rm -f "$CONTAINER_NAME"
+  fi
+
+  # Generate 'link' pages
+  echo "<html><head><meta http-equiv=\"refresh\" content=\"0;url=$BUILD_URL\"/></head></html>" > "$ARTIFACTS_DIR/jenkinsBuild.html"
+}>>"$TEST_LOG" 2>&1
+
+# Archive the test results
+chmod 600 ../artifacts.key
+chown root:root ../artifacts.key
+rsync --password-file=../artifacts.key -qPHva --relative "./$ARTIFACTS_DIR" "$LATEST_LINK_PATH" devtools@artifacts.ci.centos.org::devtools/
+ARTIFACTS_UPLOAD_EXIT_CODE=$?
+
+echo
+echo
+
+if [ $ARTIFACTS_UPLOAD_EXIT_CODE -eq 0 ]; then
+  echo "Artifacts were uploaded to http://artifacts.ci.centos.org/devtools/$ARTIFACTS_DIR"
+  echo "Test results (Allure report) can be found at http://artifacts.ci.centos.org/devtools/$ARTIFACTS_DIR/allure-report"
+else
+  echo "ERROR: Failed to upload artifacts to http://artifacts.ci.centos.org/devtools/$ARTIFACTS_DIR"
+fi
+
+echo
+echo
+
+# We do want to see that zero specs have failed
+grep "Success" "$TEST_LOG"
+export RTN_CODE=$?
+
+exit $RTN_CODE
---
+++ booster_upstream/prebooster.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+# Basic test to verify that an upstream booster can be built and deployed to OpenShift. 
+# Test relies only on oc commands to avoid the need for installing UI test harness such as Protractor
+
+# Do not display secrets
+set +x
+
+# Handle Script parameters
+
+display_usage() { 
+	echo "This script requires (6) parameters: OS project name, booster name, cluster, repo name string, OS username, OS password." 
+	echo -e "\nUsage:\n  upstream_booster.sh username-stage boosterName https://api.starter-us-east-NNN.openshift.com nodeshift-starters OS_USERNAME OS_PASSWORD\n" 
+	} 
+
+# if less than 6 arguments supplied, display usage 
+if [  $# -le 5 ] 
+	then 
+		display_usage
+		exit 1
+fi 
+ 
+# check whether user had supplied -h or --help . If yes display usage 
+if [[ ( $# == "--help") ||  $# == "-h" ]] 
+	then 
+		display_usage
+		exit 0
+fi 
+
+PROJECT_NAME=$1  # Example: username-stage
+BOOSTER_NAME=$2  # Example: nodejs-rest-http
+CLUSTER=$3       # Example: https://api.starter-us-east-2a.openshift.com
+BOOSTER_REPO=$4  # Example: nodeshift-starters
+OS_USERNAME=$5
+OS_PASSWORD=$6
+
+# Login to OpenShift oc
+echo ""
+echo "====== Login to OpenShift oc"
+oc login $CLUSTER --username=$OS_USERNAME --password=$OS_PASSWORD
+TOKEN=`oc whoami -t`
+oc login $CLUSTER --token=$TOKEN
+
+# Change to the stage project
+echo ""
+echo "====== Change to selected OSO project"
+oc project $PROJECT_NAME 
+
+# Reset the user/project environment
+echo ""
+echo "====== Cleanup the user/project environment"
+oc delete all -l app=$BOOSTER_NAME 
+oc delete build --all
+oc delete bc --all
+oc delete dc --all
+oc delete deploy --all
+oc delete is --all
+oc delete istag --all
+oc delete isimage --all
+oc delete job --all
+oc delete po --all
+oc delete rc --all
+oc delete rs --all
+oc delete statefulsets --all
+oc delete configmap --all
+oc delete services --all
+oc delete routes --all
+oc delete template --all
+
+# Create the new app
+echo ""
+echo "====== Create the new app"
+
+# Explicitly reference the boosters' application.yaml file - so as to not rely on any default values 
+# Example oc new-app command syntax:
+# oc new-app -f https://raw.githubusercontent.com/golang-starters/golang-health-check/master/.openshiftio/application.yaml 
+#    -p SOURCE_REPOSITORY_URL=https://github.com/golang-starters/golang-health-check
+
+echo "Command = oc new-app -f https://raw.githubusercontent.com/$BOOSTER_REPO/$BOOSTER_NAME/master/.openshiftio/application.yaml -p SOURCE_REPOSITORY_URL=https://github.com/$BOOSTER_REPO/$BOOSTER_NAME"
+
+oc new-app -f https://raw.githubusercontent.com/$BOOSTER_REPO/$BOOSTER_NAME/master/.openshiftio/application.yaml -p SOURCE_REPOSITORY_URL=https://github.com/$BOOSTER_REPO/$BOOSTER_NAME 
+
+
+if [[ "$BOOSTER_NAME" == *"golang"* ]]; then
+    echo "matched";
+
+    # Start the S2I build for the new app
+    echo ""
+    echo "====== Start the S2I build for the new app"
+    oc start-build $BOOSTER_NAME-s2i
+
+else
+
+    # Monitor the log for the new app
+    echo ""
+    echo "====== Watch the log for the new app build"
+    oc logs -f bc/$BOOSTER_NAME
+
+    # Expose a route for the app
+    echo ""
+    echo "====== Expose a route for/to the new app"
+    oc expose svc/$BOOSTER_NAME
+
+    # Deploy the new app
+    echo ""
+    echo "====== Deploy the new app"
+    sleep 10
+    oc rollout latest dc/$BOOSTER_NAME
+fi
+
+# Obtain the text displayed by the deployed app
+echo ""
+echo "Pause to enable the app to start, then dump text from app"
+#sleep 30
+
+RETRY_COUNTER=0
+RETRY_MAX=30
+
+# Loop - Waiting for the app to become available at its deployed endpoint
+while [  $RETRY_COUNTER -lt $RETRY_MAX ]; do
+    echo "Looking for the deployed app - The counter is $RETRY_COUNTER"
+    sleep 10
+    let RETRY_COUNTER=RETRY_COUNTER+1 
+
+    curl -s `oc get routes -o jsonpath='{range .items[*].spec}{""}{.host}{"\n"}{end}'` | grep "The application is currently not serving requests at this endpoint." 
+    if [ $? = 1 ]
+        then
+            let RETRY_COUNTER=RETRY_MAX
+            sleep 10
+    fi
+done
+         
+# Remove tokens ("grep -v integrity") from output
+curl -s `oc get routes -o jsonpath='{range .items[*].spec}{""}{.host}{"\n"}{end}'` | grep -v integrity > ${BOOSTER_NAME}_output.txt
+
+# Compare actual results to expected results
+diff ${BOOSTER_NAME}_output.txt ${BOOSTER_NAME}_expected_output.txt
+
+if [ $? = 1 ]
+    then 
+        RESULT="Fail"
+    else 
+        RESULT="Success"
+fi
+
+echo "Test results for $BOOSTER_NAME: $RESULT" | tee results.txt
+
+
+# Logout from oc
+echo ""
+echo "====== Create the new app"
+oc logout
+
+
+
