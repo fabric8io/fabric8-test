78abd3bf8e5ea4d569c8f50c8d29a89772417f48
Lucia Jelinkova <ljelinko@redhat.com>
2019-01-07 14:52:50 +0100

    E2E: Added oc logs after stage and run checks

--- ee_tests/src/interactions/deployed_application_interactions.ts
+++ ee_tests/src/interactions/deployed_application_interactions.ts
@@ -1,9 +1,11 @@
 import { browser, by, element, ExpectedConditions as until } from 'protractor';
 import * as logger from '../support/logging';
 import * as timeouts from '../support/timeouts';
+import * as runner from '../support/script_runner';
 import { windowManager } from '../support/window_manager';
 import { screenshotManager } from '../support/screenshot_manager';
 import { Environment } from '../support/environments';
+import { specContext } from '../support/spec_context';
 
 export abstract class DeployedApplicationInteractionsFactory {
 
@@ -28,27 +30,52 @@ abstract class AbstractDeployedApplicationInteractions implements DeployedApplic
 
     async verifyDeployedApplication(testCallback: () => void): Promise<void> {
         logger.info('Verify application on ' + this.environment + ' environment');
-        await windowManager.switchToNewWindow();
+        let error: any;
+        try {
+            await windowManager.switchToNewWindow();
 
-        await browser.wait(
-            until.urlContains(this.environment),
-            timeouts.DEFAULT_WAIT,
-            `url contains ${this.environment}`);
+            await browser.wait(
+                until.urlContains(this.environment),
+                timeouts.DEFAULT_WAIT,
+                `url contains ${this.environment}`);
 
-        await browser.wait(async () => {
-            await browser.refresh();
-            return ! await element(by.cssContainingText('h1', 'Application is not available')).isPresent();
-        }, timeouts.LONGER_WAIT, 'Application takes more than 10 minutes to start');
+            await browser.wait(async () => {
+                await browser.refresh();
+                return ! await element(by.cssContainingText('h1', 'Application is not available')).isPresent();
+            }, timeouts.LONGER_WAIT, 'Application takes more than 10 minutes to start');
 
-        await screenshotManager.save(this.environment);
+            let currentURL = await browser.getCurrentUrl();
+            expect(currentURL).toContain(this.environment, `${this.environment} environment url`);
 
-        let currentURL = await browser.getCurrentUrl();
-        expect(currentURL).toContain(this.environment, `${this.environment} environment url`);
+            await testCallback();
+        } catch (e) {
+            error = e;
+        } finally {
+            await screenshotManager.save(this.environment);
+            await this.runOCScript();
+            await windowManager.closeAllWindows();
+        }
 
-        await testCallback();
-
-        await windowManager.closeCurrentWindow();
+        if (error !== undefined) {
+            throw error;
+        }
     }
+
+    private async runOCScript() {
+        try {
+          logger.info(`Save OC ${this.environment} pod log`);
+          await runner.runScript(
+            '.', // working directory
+            './oc-get-project-logs.sh', // script
+            [specContext.getUser(), specContext.getPassword(), this.environment], // params
+            `./target/screenshots/oc-${this.environment}.txt`,  // output file
+            false,
+            timeouts.LONGER_WAIT
+          );
+        } catch (e) {
+          logger.info('Save OC Jenkins pod log failed with error: ' + e);
+        }
+      }
 }
 
 class StageDeployedApplicationInteractions extends AbstractDeployedApplicationInteractions {
